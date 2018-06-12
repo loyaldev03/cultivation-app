560232b61b2ca8386a563095fab29878d523ac15
Andy Yong <kwokhou@gmail.com>
2018-06-12 09:26:50 +0800

    Login layout update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :miniprofiler
+  layout :layout_by_resource
 
   private
 
   def miniprofiler
     # Enable mini profiler only if developer login
     Rack::MiniProfiler.authorize_request if current_user && current_user.is_dev?
+  end
+
+  def layout_by_resource
+    if devise_controller?
+      "login"
+    else
+      "application"
+    end
   end
 end

4f7e5f046a7943a15a97d02546e24b4aec10ac3b
Andy Yong <kwokhou@gmail.com>
2018-06-15 11:20:56 +0800

    Update Room Setup form

--- app/forms/facility_room_setup_form.rb
+++ app/forms/facility_room_setup_form.rb
@@ -1,17 +1,38 @@
 class FacilityRoomSetupForm
   include ActiveModel::Model
 
-  delegate :id, :name, :code, to: :@facility
+  delegate :id, :name, :code, :rooms, to: :facility, prefix: true
+  delegate :id, :name, :code, :desc, to: :room, prefix: true
 
-  validates :id, presence: true
-  validates :name, presence: true
-  validates :code, presence: true
+  validates :facility_id, presence: true
+  validates :facility_name, presence: true
+  validates :facility_code, presence: true
+  validates :room_id, presence: true
+  validates :room_name, presence: true
+  validates :room_code, presence: true
+  validates :room_desc, presence: true
 
-  def initialize(facility)
-    @facility = facility
+  def initialize(_facility, _room_id = nil)
+    @facility = _facility
+    @room = _room_id.nil? ? _facility.rooms.first : _facility.rooms.detect { |r| r.id.to_s == _room_id }
+  end
+
+  def facility
+    @facility
+  end
+
+  def room
+    @room
   end
 
   def submit(params)
-    return true
+    @room.name = params[:room_name]
+    @room.code = params[:room_code]
+    # TODO: Validate uniqueness of room code
+    if valid?
+      return true
+    else
+      return false
+    end
   end
 end

42eb350eb005a987a432a8f92d9a9a4b83104a57
Karg Weng Lee <karg@betacode.io>
2018-06-22 11:28:40 +0000

    Merged PR 4: added sparkpost config
    added sparkpost config
    
    Related work items: #37

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,13 +66,15 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: ENV["MAILER_URL_HOST"] }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.sendgrid.net',
-    port: '587',
-    authentication: :plain,
-    user_name: "apikey",
-    password: ENV["SENDGRID_API_KEY"],
-    enable_starttls_auto: true
+  
+  ActionMailer::Base.smtp_settings = {
+    user_name: ENV['SPARKPOST_SMTP_USERNAME'],
+    password:  ENV['SPARKPOST_SMTP_PASSWORD'],
+    address: ENV['SPARKPOST_SMTP_HOST'],
+    port: ENV['SPARKPOST_SMTP_PORT'].to_i,
+    enable_starttls_auto: true,
+    format: :html,
+    from: 'mailer@cannacted.com'
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.

f9842b26379128e54f8c35894a68e36402e2931d
Andy Yong <kwokhou@gmail.com>
2018-06-26 12:38:06 +0800

    Remove duplicate model within facility

--- app/models/row.rb
+++ app/models/row.rb
@@ -3,6 +3,7 @@ class Row
 
   field :name, type: String
   field :code, type: String
+  field :is_complete, type: Boolean, default: -> { false }
 
   embedded_in :section
   embeds_many :shelves, class_name: 'Shelf'

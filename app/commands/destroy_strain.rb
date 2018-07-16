4cb94eb35764fb8024978eb7ee789c070a3dd109
Andy <andy@betacode.io>
2018-07-16 16:42:52 +0800

    Added specs & commands for strains crud

---
+++ app/commands/destroy_strain.rb
@@ -0,0 +1,15 @@
+class DestroyStrain
+  prepend SimpleCommand
+
+  def initialize(record_id)
+    if record_id.nil?
+      raise 'Invalid record id'
+    else
+      @record_id = record_id
+    end
+  end
+
+  def call
+    @record = Strain.where(id: @record_id).delete
+  end
+end
---
+++ spec/commands/destroy_strain_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe DestroyStrain, type: :command do
+  context ".call" do
+    subject {
+      Strain.create!(
+        name: Faker::Lorem.word,
+        desc: Faker::Lorem.sentence
+      )
+    }
+    it "should delete record from db" do
+      cmd = DestroyStrain.call(subject.id.to_s)
+      
+      expect(cmd.errors).to be {}
+      expect(cmd.success?).to be true
+      expect(Strain.count).to eq 0
+    end
+  end
+end

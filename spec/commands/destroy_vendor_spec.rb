1437996a18e1f4aeedcb72c2b08864e0edb5ffec
Andy <andy@betacode.io>
2018-07-17 13:45:41 +0800

    Added Vendor CRUD commands

---
+++ spec/commands/destroy_vendor_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe DestroyVendor, type: :command do
+  context ".call" do
+    subject {
+      Vendor.create!(
+        name: Faker::Lorem.word,
+      )
+    }
+    it "should delete record from db" do
+      cmd = DestroyVendor.call(subject.id.to_s)
+      
+      expect(cmd.errors).to be {}
+      expect(cmd.success?).to be true
+      expect(Vendor.count).to eq 0
+    end
+  end
+end

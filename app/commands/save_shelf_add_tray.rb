8ec1528e9da730f93a2965713349b36d2213a745
Andy Yong <andy@betacode.io>
2018-08-03 17:10:15 +0800

    Added Add Tray on Shelf Trays page

---
+++ app/commands/save_shelf_add_tray.rb
@@ -0,0 +1,39 @@
+class SaveShelfAddTray
+  prepend SimpleCommand
+
+  def initialize(
+    facility_id,
+    room_id,
+    row_id,
+    shelf_id
+  )
+    @facility_id = facility_id
+    @room_id = room_id
+    @row_id = row_id
+    @shelf_id = shelf_id
+  end
+
+  def call
+    save_record
+  end
+
+  private
+
+  def save_record
+    facility = Facility.find(@facility_id)
+    room = facility.rooms.find(@room_id)
+    row = room.rows.find(@row_id)
+    shelf = row.shelves.find(@shelf_id)
+    # TODO: Update flag on Shelf regards using trays
+    if shelf.trays.blank?
+      last_code = Sequence.tray_code_format % 0
+    else
+      last_code = shelf.trays&.last&.code
+    end
+    tray = Tray.new
+    tray.shelf = shelf
+    tray.code = NextFacilityCode.call(:tray, last_code, 1).result
+    tray.save!
+    tray
+  end
+end

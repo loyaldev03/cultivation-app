3b1c987e58bf81e6579ad2a5b8020a603c8afc85
Allison <allison@betacode.io>
2018-08-07 16:29:18 +0800

    edit grey fonts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,9 +8,9 @@ module ApplicationHelper
       if pair.length == 2
         link_to pair[0], pair[1], class: 'link subtitle-2 orange'
       elsif pair.is_a? String
-        content_tag :span, pair, class: 'subtitle-2'
+        content_tag :span, pair, class: 'subtitle-2 grey'
       else
-        content_tag :span, pair, class: 'subtitle-2'
+        content_tag :span, pair, class: 'subtitle-2 grey'
       end
     end.join(
       content_tag :i,'keyboard_arrow_right', class: 'material-icons md-600 md-gray md-17 ph2'

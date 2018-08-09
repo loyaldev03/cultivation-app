ee5a3a44343bfaf15438722d23ea5d4db0ae2b60
Andy Yong <andy@betacode.io>
2018-08-09 11:56:32 +0800

    - Add section desc when user choose to split room
    - Fix Room Setup breadcrumb not clickable

--- app/forms/facility_wizard_form/room_summary_form.rb
+++ app/forms/facility_wizard_form/room_summary_form.rb
@@ -2,12 +2,22 @@ module FacilityWizardForm
   class RoomSummaryForm
     include Mapper
 
-    ATTRS = [:facility_id, :room_id]
+    ATTRS = [:facility_id, :room_id, :has_sections]
 
     attr_accessor(*ATTRS)
 
     def initialize(params = {})
       self.map_attrs_from_hash(ATTRS, params)
+      set_record
+    end
+
+    private
+
+    def set_record
+      find_cmd = FindFacility.call({id: self.facility_id})
+      facility = find_cmd.result
+      room = facility.rooms.find(self.room_id)
+      self.has_sections = room.has_sections
     end
   end
 end

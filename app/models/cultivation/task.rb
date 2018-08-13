bb93af8d3de42153b8b7e85b71eed0ecdb7d5013
fathiabdulrahim <fathiabdrahim@yahoo.com>
2018-08-13 16:01:27 +0800

    created cultivation table

---
+++ app/models/cultivation/task.rb
@@ -0,0 +1,24 @@
+module Cultivation
+  class Task
+    include Mongoid::Document
+    include Mongoid::Timestamps::Short
+
+    field :phase, type: String
+    field :task_category, type: String
+    field :name, type: String
+    field :days, type: Integer
+    field :days_from_start_date, type: Integer
+    field :expected_start_date, type: DateTime
+    field :expected_end_date, type: DateTime
+    field :start_date, type: DateTime
+    field :end_date, type: DateTime
+    field :expected_hours_taken, type: Float
+    field :time_taken, type: Float #actual time taken
+    field :no_of_employees, type: Integer #needed
+    field :materials, type: String # later need to integrate with real material module
+    field :instruction, type: String
+
+    embeds_many :users, class_name: 'User'
+
+  end
+end

a0e045c6718df637e2d8866cacd46090df8b9f7e
Karg Weng <karg@betacode.io>
2018-08-14 16:21:33 +0800

    Hid the unnecessary inventory settings

--- app/javascript/components/inventory/plant_setup/store/PlantStore.js
+++ app/javascript/components/inventory/plant_setup/store/PlantStore.js
@@ -2,3 +2,11 @@
 
 const plantStore = ['a', 'b']
 export default plantStore
+
+// Maybe facility store here instead with methods to
+// 1. load/ reload,
+// 2. return rooms base on facility id
+// 3. retun sections base on room id
+// 4. retun rows base on section or room id
+// 5. retun shelves base on row
+// 6. retun trays base on shelve id

37e5c1b2c87b260e2170fa866f1ed32c92154f88
fathiabdulrahim <fathiabdrahim@yahoo.com>
2018-08-14 15:40:30 +0800

    added mobx to cultivation batches tested

---
+++ app/javascript/components/cultivation/models/BatchModel.js
@@ -0,0 +1,18 @@
+import { observable, computed, action } from "mobx";
+
+class BatchModel {
+  id = Math.random();
+  @observable batch_source;
+  @observable status = "pending";
+
+  constructor() {
+  }
+
+  @action
+  update(status){
+    this.status = status
+  }
+}
+
+const batchModel = new BatchModel()
+export default batchModel

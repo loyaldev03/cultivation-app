11e8a1f487791f1ee84b998e4914f1964949228e
Allison <allison@betacode.io>
2018-08-30 14:37:48 +0800

    fix TypeError bug, cleaned up code

--- app/assets/javascripts/dropdown_menu.js
+++ app/assets/javascripts/dropdown_menu.js
@@ -1,21 +1,19 @@
 function toggleDropdown() {
-  //document.getElementById("myDropdown").classList.toggle("show");
-  $_("#myDropdown").classList.toggle("show");
+  $_('#myDropdown').classList.toggle('show')
 }
 
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener('DOMContentLoaded', function() {
   window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
-  
-      var dropdowns = $_(".dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
+      var dropdowns = $_('.dropdown-content')
+
+      for (var i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i].value;
         if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
+          openDropdown.classList.remove('show')
         }
       }
     }
   }
-});
+})
 

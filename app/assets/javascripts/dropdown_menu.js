0ab458af7c943ba2cdbd444db0a630718153e150
Allison <allison@betacode.io>
2018-09-04 13:25:54 +0800

    commented out to avoid bug

--- app/assets/javascripts/dropdown_menu.js
+++ app/assets/javascripts/dropdown_menu.js
@@ -2,18 +2,18 @@ function toggleDropdown() {
   $_('#myDropdown').classList.toggle('show')
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = $_('.dropdown-content')
+// document.addEventListener('DOMContentLoaded', function() {
+//   window.onclick = function(event) {
+//     if (!event.target.matches('.dropbtn')) {
+//       var dropdowns = $_('.dropdown-content')
 
-      for (var i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i].val();
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show')
-        }
-      }
-    }
-  }
-})
+//       for (var i = 0; i < dropdowns.length; i++) {
+//         var openDropdown = dropdowns[i].val();
+//         if (openDropdown.classList.contains('show')) {
+//           openDropdown.classList.remove('show')
+//         }
+//       }
+//     }
+//   }
+// })
 

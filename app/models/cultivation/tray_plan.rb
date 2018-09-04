0027857112cf5146062b5eeb3ec470f9fb9d6f79
Andy Yong <andy@betacode.io>
2018-09-04 14:25:33 +0800

    Update ref field into BSON::ObjectId

--- app/models/cultivation/tray_plan.rb
+++ app/models/cultivation/tray_plan.rb
@@ -3,11 +3,11 @@ module Cultivation
     include Mongoid::Document
     include Mongoid::Timestamps::Short
 
-    field :facility_id, type: String
-    field :room_id, type: String
-    field :row_id, type: String
-    field :shelf_id, type: String
-    field :tray_id, type: String
+    field :facility_id, type: BSON::ObjectId
+    field :room_id, type: BSON::ObjectId
+    field :row_id, type: BSON::ObjectId
+    field :shelf_id, type: BSON::ObjectId
+    field :tray_id, type: BSON::ObjectId
     field :capacity, type: Integer
     field :phase, type: String                  # cultivation phase
     field :is_active, default: -> { true }      # indicate the record is still in-use.

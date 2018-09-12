022984ba8447aa6de3394a560d173d3e3bd05cec
fathiabdulrahim <fathiabdrahim@yahoo.com>
2018-09-12 16:05:50 +0800

    added addTaskForm, and button for adding task form

---
+++ app/javascript/components/cultivation/tasks_setup/components/AddTaskForm.js
@@ -0,0 +1,234 @@
+import React from 'react'
+import { render } from 'react-dom'
+import TaskStore from '../stores/TaskStore'
+import DatePicker from 'react-date-picker/dist/entry.nostyle'
+import Select from 'react-select'
+import { TextInput, FieldError, NumericInput } from '../../../utils/FormHelpers'
+import { fadeToast, toast } from '../../../utils/toast'
+import reactSelectStyle from './../../../utils/reactSelectStyle'
+import { throws } from "assert";
+import updateTasks from '../actions/updateTask'
+
+
+class AddTaskForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      parent_task: props.parent_task,
+      parent_id: props.id,
+      name: '',
+      days: '',
+      task_category: props.parent_task.attributes.task_category,
+      instruction: '',
+      start_date: new Date(),
+      end_date: new Date(),
+      errors: '',
+      estimated_hours: '',
+      assigned_employee: []
+    }
+  }
+
+  componentWillReceiveProps(props) {
+    const { parent_id } = this.props.id
+    if (props.id !== parent_id) {
+      this.setState({
+        parent_id: props.id,
+        name: '',
+        days: '',
+        task_category: props.parent_task.attributes.task_category,
+        instruction: '',
+        start_date: new Date(),
+        end_date: new Date(),
+        errors: '',
+        estimated_hours: '',
+        assigned_employee: []
+      })
+    }
+  }
+
+  handleChangeTask = event => {
+    // console.log(event[0].value)
+    let key = event.target.attributes.fieldname.value
+    let value = event.target.value
+    this.setState({ [key]: value })
+  }
+
+  handleChangeDate = (key, value) => {
+    console.log(value)
+    this.setState({ [key]: value })
+  }
+
+  handleChangeSelect = (value, { action, removedValue }) => {
+    console.log(value)
+    let arr = this.state.assigned_employee
+    switch (action) {
+      case 'select-option':
+        // arr.push(value[0])
+        // console.log(value[0].value)
+        break
+      case 'remove-value':
+        // console.log(removedValue.value)
+        const index = arr.indexOf(removedValue)
+        arr.splice(index, 1)
+        break
+    }
+    this.setState({ assigned_employee: value })
+    // value = orderOptions(value);
+    // this.setState({ value: value });
+  }
+
+  handleSubmit = (event) => {
+    window.editorSidebar.close()
+
+    toast('Task Created', 'success')
+
+    // updateTasks.updateTask(this.state)
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="ph4 mt3 mb3 flex">
+          <div className="w-60">
+            <TextInput
+              label={'Task'}
+              value={this.state.name}
+              onChange={this.handleChangeTask}
+              fieldname="name"
+              errors={this.state.errors}
+              errorField="name"
+            />
+          </div>
+          <div className="w-40 pl3">
+            <TextInput
+              label={'Category'}
+              value={this.state.task_category}
+              onChange={this.handleChangeTask}
+              fieldname="task_category"
+              errors={this.state.errors}
+              errorField="task_category"
+              readOnly={true}
+            />
+          </div>
+        </div>
+
+        <div className="ph4 mt3 mb3 flex">
+          <div className="w-100">
+            <label className="f6 fw6 db mb1 gray ttc">Instruction</label>
+            <textarea
+              value={this.state.instruction}
+              onChange={this.handleChangeTask}
+              fieldname="instruction"
+              rows="5"
+              className="db w-100 pa2 f6 black ba b--black-20 br2 mb0 outline-0 lh-copy"
+              placeholder="Plant0001, Tray0001&#10;Plant0002, Tray0001&#10;Plant0003, Tray0002&#10;Plant0004, Tray0002"
+            />
+            <FieldError errors={this.state.errors} fieldname="instruction" />
+          </div>
+        </div>
+        <div className="ph4 mt3 mb3 flex">
+
+          <div className="w-50">
+            <label className="f6 fw6 db mb1 gray ttc">Start Date</label>
+            <DatePicker
+              value={this.state.start_date}
+              fieldname="start_date"
+              onChange={e => this.handleChangeDate('start_date', e)}
+            />
+          </div>
+
+          <div className="w-50 pl3">
+            <label className="f6 fw6 db mb1 gray ttc">End Date</label>
+            <DatePicker
+              value={this.state.end_date}
+              fieldname="end_date"
+              onChange={e => this.handleChangeDate('end_date', e)}
+            />
+          </div>
+        </div>
+
+        <div className="ph4 mt3 mb3 flex">
+          <div className="w-20">
+            <NumericInput
+              label={'Days'}
+              value={this.state.days}
+              onChange={this.handleChangeTask}
+              fieldname="days"
+              errors={this.state.errors}
+              errorField="days"
+            />
+          </div>
+
+          <div className="w-40 pl3">
+            <NumericInput
+              label={'Estimated Hours Needed'}
+              value={this.state.estimated_hours}
+              onChange={this.handleChangeTask}
+              fieldname="estimated_hours"
+              errors={this.state.errors}
+              errorField="estimated_hours"
+            />
+          </div>
+        </div>
+
+        <div className="ph4 mt3 mb3 flex">
+          <div className="w-60">
+            <label className="f6 fw6 db mb1 gray ttc">Assigned Employees</label>
+            <Select
+              isMulti
+              name="colors"
+              options={[
+                { value: 'Fathi', label: 'Fathi' },
+                { value: 'Andy', label: 'Andy' },
+                { value: 'Karg', label: 'Karg' },
+                { value: 'Allison', label: 'Allison' }
+              ]}
+              className="basic-multi-select"
+              classNamePrefix="select"
+              fieldname="assigned_employee"
+              onChange={this.handleChangeSelect}
+              value={this.state.assigned_employee}
+              styles={reactSelectStyle}
+            />
+          </div>
+        </div>
+
+        <div className="ph4 mt3 mb3 flex">
+          <div className="w-60">
+            <label className="f6 fw6 db mb1 gray ttc">Material Suggested</label>
+            <Select
+              isMulti
+              name="colors"
+              options={[
+                { value: 'Fathi', label: 'Fathi' },
+                { value: 'Andy', label: 'Andy' },
+                { value: 'Karg', label: 'Karg' },
+                { value: 'Allison', label: 'Allison' }
+              ]}
+              className="basic-multi-select"
+              classNamePrefix="select"
+              styles={reactSelectStyle}
+            />
+          </div>
+        </div>
+
+        {/* <div className="w-100 flex justify-end">
+          <a className="pv2 ph3 bg-orange white bn br2 ttu tracked link dim f6 fw6 pointer" onClick={this.handleSubmit}>Submit</a>
+        </div> */}
+        <div className="w-100 pa4 bt b--light-grey absolute right-0 bottom-0 flex items-center justify-between">
+          <button
+            name="commit"
+            type="submit"
+            value="continue"
+            className="ttu db tr pa3 bg-orange button--font white bn box--br3 ttu link dim pointer"
+            onClick={this.handleSubmit}
+          >
+            Create &amp; Close
+          </button>
+        </div>
+      </React.Fragment>
+    )
+  }
+}
+
+export default AddTaskForm
--- app/javascript/components/cultivation/tasks_setup/components/TaskList.js
+++ app/javascript/components/cultivation/tasks_setup/components/TaskList.js
@@ -25,6 +25,34 @@ class TaskList extends React.Component {
     window.editorSidebar.close()
   }
 
+  renderAddButton = (row) => {
+    if (row.value == true){
+      return (
+      <div>
+        <i className="material-icons md-600 md-gray md-17 ph2 cursor-pointer" onClick={(e) => {this.handleAddTask(row)}}>add</i>
+      </div>
+      )
+    }
+  }
+
+  renderPhaseColumn = (row) => {
+    if (row.row['attributes.isPhase'] == true) {
+      return (<div>{row.value}</div>)
+    }
+  }
+
+  renderCategoryColumn = (row) => {
+    if (row.row['attributes.isCategory'] == true) {
+      return (<div>{row.value}</div>)
+    }
+  }
+
+  handleAddTask = (row) => {
+    console.log(row.row.id)
+    editorSidebarHandler.open({ width: '500px', data: {id: row.row.id}, action: 'add' })
+
+  }
+
   render() {
     let tasks = TaskStore
     return (
@@ -36,21 +64,33 @@ class TaskList extends React.Component {
               Header: 'Id',
               accessor: 'id',
               maxWidth: '100',
-              show: true
+              show: false
+            },
+            {
+              Header: '',
+              accessor: 'attributes.isCategory',
+              maxWidth: '50',
+              id: 'button-column',
+              Cell: (row) => (<div>{this.renderAddButton(row)}</div>)
             },
             {
               Header: 'Phase',
               accessor: 'attributes.phase',
-              maxWidth: '100'
+              maxWidth: '100',
+              Cell: (row) => (<div>{this.renderPhaseColumn(row)}</div>)
             },
             {
               Header: 'Category',
               accessor: 'attributes.task_category',
-              maxWidth: '100'
+              maxWidth: '100',
+              Cell: (row) => (<div>{this.renderCategoryColumn(row)}</div>)
+
             },
             {
               Header: 'Name',
-              accessor: 'attributes.name'
+              accessor: 'attributes.name',
+              maxWidth: '300'
+
             },
             {
               Header: 'Start Date',
@@ -68,43 +108,45 @@ class TaskList extends React.Component {
               maxWidth: '100'
 
             },
-            // {
-            //   Header: "Parent",
-            //   accessor: "attributes.parent_id"
+            {
+              Header: "Parent",
+              accessor: "attributes.parent_id",
+              show: false
 
-            // },
-            // {
-            //   Header: "Depend On",
-            //   accessor: "attributes.depend_on"
-            // },
-            // {
-            //   Header: "Category?",
-            //   accessor: "attributes.isCategory",
-            // },
-            // {
-            //   Header: "Phase?",
-            //   accessor: "attributes.isPhase",
-            // }
+            },
+            {
+              Header: "Depend On",
+              accessor: "attributes.depend_on",
+              show: false
+            },
+            {
+              Header: "Category?",
+              accessor: "attributes.isCategory",
+              show: false
+            },
+            {
+              Header: "Phase?",
+              accessor: "attributes.isPhase",
+              show: false
+            }
           ]}
           data={tasks}
           rows={100}
           className="-striped -highlight"
           defaultPageSize={100}
-          getTrProps={(state, rowInfo, column) => {
-            if (rowInfo) {
-              return {
-                style: {
-                  // background: rowInfo.row['attributes.isPhase'] == "true" ? '#bdbdbd' : null,
-                },
-                onClick: (e, handleOriginal) => {
-                  // console.log(handleOriginal)
-                  editorSidebarHandler.open({ width: '500px', data: rowInfo.row })
+          getTdProps={(state, rowInfo, column, instance)=>{
+              if (rowInfo) {
+                return {
+                  onClick: (e, handleOriginal) => {
+                    if (column.id != "button-column" ){
+                      editorSidebarHandler.open({ width: '500px', data: rowInfo.row, action: 'update' })
+                    }
+                  }
                 }
               }
+              return {}
             }
-            return {}
           }
-        }
         />
         <TaskEditor onClose={this.closeSidebar} batch_id={this.props.batch_id} />
       </React.Fragment>

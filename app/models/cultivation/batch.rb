bd0051e60925745e9e87daf294c8519e03ffd23c
Andy Yong <andy@betacode.io>
2018-09-19 12:39:17 +0800

    Fix Batch <> Sstrain relations

--- app/models/cultivation/batch.rb
+++ app/models/cultivation/batch.rb
@@ -6,14 +6,14 @@ module Cultivation
     field :name, type: String
     field :batch_no, type: String
     field :batch_source, type: String
-    field :strain_id, type: BSON::ObjectId   # TODO: Change this to strain ID
     field :start_date, type: DateTime
     field :facility_id, type: BSON::ObjectId
     field :grow_method, type: String
 
     has_one :tray_plan, class_name: 'Cultivation::TrayPlan'
     has_many :tasks, class_name: 'Cultivation::Task'
-
+    belongs_to :strain, class_name: 'Common::Strain'
+    
     def phases
       tasks.where(isPhase: true)
     end

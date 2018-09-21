63745f0d9e8c6ddb5127e6261ecf5c2534cd33e7
Andy Yong <andy@betacode.io>
2018-09-21 15:15:59 +0800

    Fix timezone during api calls

--- app/commands/cultivation/find_batch_phase.rb
+++ app/commands/cultivation/find_batch_phase.rb
@@ -10,11 +10,19 @@ module Cultivation
     def call
       if @batch.present? && @batch.tasks.present? && @phase.present?
         phase_task = @batch.tasks
-          .where({isPhase: true, phase: @phase})
+          .where({is_phase: true, phase: @phase})
           .order_by(start_date: :asc)
           .first
         if phase_task.present?
-          return phase_task
+          start_date = phase_task.start_date.beginning_of_day
+          end_date = (phase_task.end_date - 1.days).end_of_day
+          return OpenStruct.new({
+                   id: phase_task.id,
+                   name: phase_task.name,
+                   start_date: start_date,
+                   duration: phase_task.duration,
+                   end_date: end_date,
+                 })
         else
           errors.add(:not_found, 'Record Not Found')
         end

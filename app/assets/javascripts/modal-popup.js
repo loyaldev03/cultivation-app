6acb4f8c14e758b83b768a878b56c27a9d493f44
Allison <allison@betacode.io>
2018-09-25 11:20:38 +0800

    working on popup modal

--- app/assets/javascripts/modal-popup.js
+++ app/assets/javascripts/modal-popup.js
@@ -1,11 +1,20 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+function hideModal() {
+  console.log("close")
+   $_('.modal').classList.add("dn");
+}
+
+document.addEventListener('DOMContentLoaded', function(event) {
   var modal = $_('#myModal');
+  const targetCard = $('#notification-popup');
+  const modalCard = $('.modal'); 
 
-  $_('#notification-popup').on("click", function() {
-    modal.style.display = 'block';
+  $_('#notification-popup').on('click', function() {
+    modal.style.display = 'block'
   })
 
-  $_('#close-modal').on("click", function() {
-    modal.style.display = "none";
-  })
+  // $_('#close-modal').on("click", function() {
+  //   console.log("close")
+  //   //modal.style.display = "none";
+  //   $_('.modal').classList.add("dn");
+  // })
 })

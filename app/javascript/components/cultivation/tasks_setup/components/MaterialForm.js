64c20a43655ae94882c816cc1542b22829c94b22
fathiabdulrahim <fathiabdrahim@yahoo.com>
2018-10-08 13:06:45 +0800

    added material table added and function to delete material from table

--- app/javascript/components/cultivation/tasks_setup/components/MaterialForm.js
+++ app/javascript/components/cultivation/tasks_setup/components/MaterialForm.js
@@ -3,7 +3,22 @@ import { render } from 'react-dom'
 import Select from 'react-select'
 
 import { TextInput, FieldError, NumericInput } from '../../../utils/FormHelpers'
-import reactSelectStyle from './../../../utils/reactSelectStyle'
+
+
+const uom_dropdown = [
+  { value: 'Fathi', label: 'KG' },
+  { value: 'Andy', label: 'CM' },
+  { value: 'Karg', label: 'Inch' },
+  { value: 'Allison', label: 'ML' }
+]
+
+const material_dropdown = [
+  { value: 'Fathi', label: 'Fathi' },
+  { value: 'Andy', label: 'Andy' },
+  { value: 'Karg', label: 'Karg' },
+  { value: 'Allison', label: 'Allison' }
+]
+
 
 export default class MaterialForm extends React.Component {
   constructor(props) {
@@ -11,7 +26,13 @@ export default class MaterialForm extends React.Component {
     this.state = {
       batch_id: this.props.batch_id,
       id: props.task.id,
-      ...props.task.attributes
+      ...props.task.attributes,
+      material_name: '',
+      quantity: '',
+      uom: '',
+      uom_dropdown: uom_dropdown,
+      material_dropdown: material_dropdown,
+      materials: []
     }
   }
 
@@ -26,44 +47,113 @@ export default class MaterialForm extends React.Component {
     }
   }
 
+  handleChangeSelect = (key, value) => {
+    this.setState({ [key]: value })
+  }
+
+  handleChange = (key, value) => {
+    console.log(key)
+
+    console.log(value)
+    this.setState({ [key]: value.target.value })
+  }
+
+  handleSubmit = (e) => {
+    this.setState(prevState => ({
+      materials: [
+        ...prevState.materials,
+        {
+          material_name: this.state.material_name.value,
+          quantity: this.state.quantity,
+          uom: this.state.uom.label
+        }
+      ],
+      material_name: '',
+      quantity: '',
+      uom: ''
+    }))
+  }
+
+  handleDelete = id => {
+    let materials = this.state.materials
+    materials.splice(id, 1)
+    this.setState({
+      materials: materials
+    })
+  }
+
   render() {
+    let material_dropdown = this.state.material_dropdown
+    let uom_dropdown = this.state.uom_dropdown
+    let materials = this.state.materials
+    let handleChange = this.handleChange
+    let handleDelete = this.handleDelete
     return (
-      <div className="ph4 mt3 mb3 flex">
-        <div className="w-40">
-          <label className="f6 fw6 db mb1 gray ttc">Material Name</label>
-          <Select
-            isMulti
-            name="colors"
-            options={[
-              { value: 'Fathi', label: 'Fathi' },
-              { value: 'Andy', label: 'Andy' },
-              { value: 'Karg', label: 'Karg' },
-              { value: 'Allison', label: 'Allison' }
-            ]}
-            className="basic-multi-select"
-            classNamePrefix="select"
-            styles={reactSelectStyle}
+      <div className="ba b--light-gray ml4 mr4 mt4">
+        <div className="ph4 mt3 flex">
+          <div className="w-100">
+            <label className="f6 fw6 db mb1 gray ttc">Material Name</label>
+            <Select
+              name="material_name"
+              options={material_dropdown}
+              onChange={e => this.handleChangeSelect('material_name', e)}
+              value={this.state.material_name}
+            />
+          </div>
+        </div>
+        <div className="ph4 mt3 flex">
+
+          <div className="w-100">
+            <NumericInput
+              label={'Quantity'}
+              value={this.state.quantity}
+              onChange={e => handleChange('quantity', e)}
+              fieldname="quantity"
+              errors={this.state.errors}
+              errorField="quantity"
+            />
+          </div>
+        </div>
+        <div className="ph4 mt3 flex mb3 ">
+          <div className="w-100">
+            <label className="f6 fw6 db mb1 gray ttc">Unit Of Measure</label>
+            <Select
+              name="uom"
+              options={uom_dropdown}
+              onChange={e => this.handleChangeSelect('uom', e)}
+              value={this.state.uom}
+            />
+          </div>
+        </div>
+        <div className="pv2 w4">
+          <input
+            type="submit"
+            className="pv2 ph3 ml4 bg-orange white bn br2 ttu tc tracked link dim f6 fw6 pointer"
+            value="Add Material"
+            onClick={this.handleSubmit}
           />
         </div>
-        <div className="w-30 pl3">
-          <NumericInput
-            label={'Quantity'}
-            value={this.state.quantity}
-            onChange={this.handleChangeTask}
-            fieldname="quantity"
-            errors={this.state.errors}
-            errorField="quantity"
-          />
-        </div>
-        <div className="w-30 pl3">
-          <TextInput
-            label={'Unit Of Measure'}
-            value={this.state.uom}
-            onChange={this.handleChangeTask}
-            fieldname="uom"
-            errors={this.state.errors}
-            errorField="uom"
-          />
+        <div className="mt4 mr4 ml4 f6 fw6 db mb1 gray ttc">
+          {this.state.materials.length !== 0 ? <span>Materials Added</span> : null}
+          <table className="w-100">
+            <tbody>
+              {materials.map((x, index) => (
+                <tr className="pointer bb" key={index}>
+                  <td className="tl pv2 ph3">{x.material_name}</td>
+                  <td className="tl pv2 ph3">{x.quantity}</td>
+                  <td className="tl pv2 ph3">{x.uom}</td>
+                  <td className="tl pv2 ph3">
+                    <i
+                      className="material-icons red md-18 pointer dim"
+                      onClick={e => handleDelete(index)}
+                    >
+                      delete
+                    </i>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     )

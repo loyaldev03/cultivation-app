fd1ce5f0108a35c686f976f4bc4bcf772ea2c67a
andy <andy@betacode.io>
2018-10-16 17:43:28 +0800

    Show strain update

--- app/javascript/components/cultivation/batches_setup/BatchSetupEditor.js
+++ app/javascript/components/cultivation/batches_setup/BatchSetupEditor.js
@@ -1,14 +1,31 @@
 import React from 'react'
+import Select from 'react-select'
 import { formatDate } from './../../utils'
 
 class BatchSetupEditor extends React.PureComponent {
   render() {
-    const { date } = this.props
+    const { date, strains, growMethods, onChange, submit } = this.props
     return (
       <div>
         <div>{formatDate(date)}</div>
         <div></div>
         <div>Strain</div>
+        <Select
+          options={strains}
+          onChange={e => onChange('strain', e.value)}
+        />
+        <Select
+          options={growMethods}
+          onChange={e => onChange('grow_method', e.value)}
+        />
+        <div className="w-100 flex justify-end mt3">
+          <a
+            className="pv2 ph3 bg-orange white bn br2 ttu tracked link dim f6 fw6 pointer"
+            onClick={submit}
+          >
+            Save
+          </a>
+        </div>
       </div>
     )
   }

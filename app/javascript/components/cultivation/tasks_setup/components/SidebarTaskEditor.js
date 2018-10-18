6ba69298588a8e85aaf76c52b967c11950164900
fathiabdulrahim <fathiabdrahim@yahoo.com>
2018-10-18 15:10:26 +0800

    run formatter

--- app/javascript/components/cultivation/tasks_setup/components/SidebarTaskEditor.js
+++ app/javascript/components/cultivation/tasks_setup/components/SidebarTaskEditor.js
@@ -212,7 +212,9 @@ class SidebarTaskEditor extends React.Component {
             <hr class="mt3 m b--light-gray w-100" />
 
             <div className="ph4 mt3 mb3">
-              <label className="f6 fw6 db mb1 ttc">Please select special task that related to daily task</label>
+              <label className="f6 fw6 db mb1 ttc">
+                Please select special task that related to daily task
+              </label>
               <label className="f6 fw6 db mb1 gray ttc">
                 <input
                   type="checkbox"

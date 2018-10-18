7448fa333b15203290685c24e5c524a2a569368c
Andy Yong <kwokhou@gmail.com>
2018-10-18 11:47:12 +0800

    Update SeedFaciltyDataJob spec

--- spec/jobs/seed_facility_data_job_spec.rb
+++ spec/jobs/seed_facility_data_job_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe SeedFacilityDataJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  include ActiveJob::TestHelper
+
+  after do
+    clear_enqueued_jobs
+    clear_performed_jobs
+  end
+
+  subject(:job) { described_class.perform_later(facility_id) }
+
+  let(:facility_id) { (BSON::ObjectId.new).to_s }
+
+  it 'queues the job' do
+    expect { job }.to have_enqueued_job(described_class)
+      .with(facility_id)
+      .on_queue("default")
+  end
+
+  it 'executes perform' do
+    perform_enqueued_jobs do
+      job
+
+      expect(Common::Role.where(built_in: true).count).to eq 1
+    end
+  end
 end

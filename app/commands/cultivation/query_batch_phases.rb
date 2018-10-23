ff4c605340401ae799ddc81e3eaae80290645300
andy <andy@betacode.io>
2018-10-23 14:46:07 +0800

    Add quantity to Cultivation::Batch model

---
+++ app/commands/cultivation/query_batch_phases.rb
@@ -0,0 +1,34 @@
+module Cultivation
+  class QueryBatchPhases
+    prepend SimpleCommand
+
+    def initialize(batch, phases = [])
+      @batch = batch
+      @phases = phases
+    end
+
+    def call
+      if @batch.present? && @batch.tasks.present?
+        tasks = @batch.tasks.where({is_phase: true})
+        tasks = tasks.where(:phase.in => @phases) if @phases.present? && @phases.any?
+        tasks = tasks.order_by(start_date: :asc)
+
+        phases = tasks.each do |task|
+          start_date = task.start_date.beginning_of_day
+          end_date = (task.end_date - 1.days).end_of_day
+          return OpenStruct.new({
+                   id: task.id,
+                   name: task.name,
+                   phase: task.phase,
+                   start_date: start_date,
+                   end_date: end_date,
+                   duration: task.duration,
+                 })
+        end
+      else
+        errors.add(:not_found, 'Incomplete Batch Record')
+        []
+      end
+    end
+  end
+end

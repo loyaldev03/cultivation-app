27f308a8c539770193ef76a45797447c50c13823
fathiabdulrahim <fathiabdrahim@yahoo.com>
2018-11-02 17:42:43 +0800

    fixes vendors crud

--- app/commands/inventory/save_vendor.rb
+++ app/commands/inventory/save_vendor.rb
@@ -37,12 +37,12 @@ module Inventory
     end
 
     def valid?
-      errors.add(:name, 'Vendor name cannot be empty') if args[:name].blank?
-      if args[:vendor_type] == 'plant_supplier'
-        errors.add(:state_license_num, 'State license is required') if args[:state_license_num].blank?
-        errors.add(:state_license_expiration_date, 'State license is required') if args[:state_license_expiration_date].blank?
-        errors.add(:location_license_num, 'Location license is required') if args[:location_license_num].blank?
-        errors.add(:location_license_expiration_date, 'Location license is required') if args[:location_license_expiration_date].blank?
+      errors.add(:name, 'Vendor name cannot be empty') if args['name'].blank?
+      if args['vendor_type'] == 'plant_supplier'
+        errors.add(:state_license_num, 'State license is required') if args['state_license_num'].blank?
+        errors.add(:state_license_expiration_date, 'State license is required') if args['state_license_expiration_date'].blank?
+        errors.add(:location_license_num, 'Location license is required') if args['location_license_num'].blank?
+        errors.add(:location_license_expiration_date, 'Location license is required') if args['location_license_expiration_date'].blank?
       end
       errors.empty?
     end

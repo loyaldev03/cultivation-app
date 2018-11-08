6c96c12118a8ab38abf0b6b1f19078a814854a19
Andy Yong <kwokhou@gmail.com>
2018-11-08 16:30:06 +0800

    Update QueryFacilityStrains with Facility filter
    - filter by facility
    - return strain_type

--- app/commands/inventory/query_facility_strains.rb
+++ app/commands/inventory/query_facility_strains.rb
@@ -1,13 +1,20 @@
 module Inventory
   class QueryFacilityStrains
     prepend SimpleCommand
+    def initialize(facility_id = nil)
+      @facility_id = facility_id
+    end
 
     def call
-      Inventory::FacilityStrain.includes(:facility).all.map do |x|
+      query = Inventory::FacilityStrain.includes(:facility)
+      query = query.where(facility_id: @facility_id) if @facility_id
+
+      query.map do |x|
         {
           value: x.id.to_s,
           label: "#{x.strain_name} - (#{x.facility.name})",
           strain_name: x.strain_name,
+          strain_type: x.strain_type,
           facility_id: x.facility_id.to_s,
         }
       end

bc73abb84b5fc28cf24cb5e01f312e66ade85c60
fathiabdulrahim <fathiabdrahim@yahoo.com>
2018-11-14 12:04:49 +0800

    fix days count in frontend

--- app/javascript/components/cultivation/tasks_setup/components/SidebarTaskEditor.js
+++ app/javascript/components/cultivation/tasks_setup/components/SidebarTaskEditor.js
@@ -74,7 +74,7 @@ class SidebarTaskEditor extends React.Component {
     let value = event.target.value
     if (key === 'duration' && value && this.state.start_date) {
       let new_end_date = new Date(this.state.start_date)
-      new_end_date.setDate(new_end_date.getDate() + parseInt(value))
+      new_end_date.setDate(new_end_date.getDate() + parseInt(value) - 1)
       this.setState({ end_date: new_end_date, [key]: value })
     } else {
       this.setState({ [key]: value })
@@ -85,11 +85,11 @@ class SidebarTaskEditor extends React.Component {
     if (key === 'end_date' && this.state.start_date) {
       let one_day = 1000 * 60 * 60 * 24
       let duration = new Date(value) - new Date(this.state.start_date)
-      this.setState({ [key]: value, duration: duration / one_day })
+      this.setState({ [key]: value, duration: (duration / one_day) +1 })
     } else if (key === 'start_date' && this.state.end_date) {
       let one_day = 1000 * 60 * 60 * 24
       let duration = new Date(this.state.end_date) - new Date(value)
-      this.setState({ [key]: value, duration: duration / one_day })
+      this.setState({ [key]: value, duration: (duration / one_day) +1 })
     } else {
       this.setState({ [key]: value })
     }

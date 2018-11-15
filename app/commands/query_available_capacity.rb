1e40dbda6f78886f44ee9cd74efe669f46ec856b
Andy Yong <kwokhou@gmail.com>
2018-11-15 15:18:58 +0800

    Quantity is unnecessary

--- app/commands/query_available_capacity.rb
+++ app/commands/query_available_capacity.rb
@@ -8,21 +8,18 @@ class QueryAvailableCapacity
       :phase => nil,            # String, Phase (e.g. :clone, :veg1, :flower)
       :start_date => nil,       # Date object, Start Date of phase
       :end_date => nil,         # Date object, End Date of phase
-      :quantity => 0,           # Integer, Capacity required by the Batch (Batch.quantity)
     }.merge(args)
 
     raise ArgumentError, 'facility_id' if args[:facility_id].nil?
     raise ArgumentError, 'start_date' if args[:start_date].nil?
     raise ArgumentError, 'end_date' if args[:end_date].nil?
     raise ArgumentError, 'start_date should be ealier than end_date' if args[:end_date] < args[:start_date]
-    raise ArgumentError, 'quantity' if args[:quantity] <= 0
 
     @facility_id = args[:facility_id]
     @exclude_batch_id = args[:exclude_batch_id]
     @phase = args[:phase]
     @start_date = args[:start_date]
     @end_date = args[:end_date]
-    @quantity = args[:quantity]
   end
 
   # Check if a cultivation phase schedule is overlaping with other batches.

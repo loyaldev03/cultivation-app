a5dfa88f8c9de6ee32ff13baccdaa6ed412d1330
Andy Yong <kwokhou@gmail.com>
2018-11-23 12:32:31 +0800

    Batch & Parent Task end date extending - Fix child task extend parent task end date - Fix batch harvest date not updating

--- app/commands/cultivation/update_task.rb
+++ app/commands/cultivation/update_task.rb
@@ -15,38 +15,54 @@ module Cultivation
       elsif @args[:type] == 'resource'
         task.update(user_ids: @args[:user_ids])
       else
-        Rails.logger.debug 'args >>> '
-        Rails.logger.debug args.to_yaml
         batch = Cultivation::Batch.includes(:tasks).find(task.batch_id)
         batch_tasks = batch.tasks
-        Rails.logger.debug ">>> number of tasks: #{batch_tasks.size}"
         task.assign_attributes(@args)
-        # Update start_date, end_date & duration of current task
-        set_task_dates(task, task.start_date)
         # Update child and dependents tasks's start & end dates
         update_task(task, batch_tasks)
-        # Extend end date to Category and Phase
+        # Extend end date to Category and Phas
         update_tasks_end_date(task, batch_tasks)
-
-        Rails.logger.debug errors.to_yaml
-        Rails.logger.debug 'task.changes >>> '
-        Rails.logger.debug task.changes.to_yaml
-        task.save unless errors.present?
-
-        # Update estimated harvest date
-        # FIXME: Fallback to :cure also if :dry not found
-        dry_phase = batch.tasks.find_by(is_phase: true, phase: 'dry')
-        batch.estimated_harvest_date = dry_phase.start_date if dry_phase
-        batch.start_date = batch.tasks.first.start_date if !batch.tasks.empty?
-        batch.save
+        # Update batch
+        update_batch(batch, batch_tasks&.first)
       end
       task
     end
 
-    # TODO: This partially working, sometime not extending parent's end_date
+    def update_batch(batch, first_task)
+      # Here we assume estimated harvest date is the start date of :dry phase
+      # (or :cure, when :dry not found)
+      harvest_phase = Cultivation::Task.
+        where(batch_id: batch.id,
+              is_phase: true,
+              :phase.in => [Constants::CONST_DRY,
+                            Constants::CONST_CURE]).first
+      batch.estimated_harvest_date = harvest_phase.start_date if harvest_phase
+      batch.start_date = first_task.start_date if first_task
+      batch.save
+    end
+
+    def update_task(task, batch_tasks, opt = {})
+      # Rails.logger.debug "`update_task method: #{task.name}`"
+      # Rails.logger.debug "`start_date: #{task.start_date}`"
+      # Rails.logger.debug "`end_date: #{task.end_date}`"
+      # Rails.logger.debug "`duration: #{task.duration}`"
+      # if date is changes, start_date, end_date and duration
+      # Store changes into a an array for 'bulk' update later
+      opt = {start_date: task.start_date}.merge(opt)
+      tasks_changes = find_changes(task, batch_tasks, opt)
+      # Rails.logger.debug "`total changes found: #{tasks_changes.length}`"
+
+      if valid_data?(tasks_changes)
+        bulk_update(tasks_changes) # bulk update
+        task
+      end
+      task
+    end
+
     def update_tasks_end_date(task, batch_tasks)
       parent = batch_tasks.detect { |t| t.id.to_s == task.parent_id.to_s }
       if parent
+        # Get all sibling tasks, including current task
         siblings = get_siblings(task, batch_tasks)
         # Get siblings max end_date (latest date)
         max_end_date = siblings.map(&:end_date).compact.max
@@ -71,32 +87,93 @@ module Cultivation
       end
     end
 
-    # Find all siblings tasks
-    def get_siblings(task, batch_tasks)
-      batch_tasks.select do |t|
-        t.parent_id &&
-          # Sibling should have same parent
-          t.parent_id.to_s == task.parent_id.to_s
-      end
-    end
-
     def update_position(task, position)
       task.move_to! position
     end
 
-    def update_task(task, batch_tasks, opt = {})
-      Rails.logger.debug "`update_task method: #{task.name}`"
-      Rails.logger.debug "`start_date: #{task.start_date}`"
-      Rails.logger.debug "`end_date #{task.end_date}`"
-      Rails.logger.debug "`duration #{task.duration}`"
-      # if date is changes, start_date, end_date and duration
-      # Store changes into a an array for 'bulk' update later
-      tasks_changes = find_changes(task, batch_tasks, opt)
-      if valid_data?(tasks_changes)
-        bulk_update(tasks_changes) # bulk update
-        task
+    def find_changes(task, batch_tasks = [], opt = {})
+      opt = {
+        start_date: nil,
+        children: true,
+        dependent: true,
+      }.merge(opt) # default options
+      raise ArgumentError, 'start_date' if opt[:start_date].nil?
+
+      # Rails.logger.debug ">>> finding changes for #{task.name}"
+      # Rails.logger.debug ">>> start_date: #{opt[:start_date]}"
+
+      # Array to store changed tasks for current iteration
+      new_changes = []
+
+      # Update start_date, end_date & duration of current task
+      set_task_dates(task, opt[:start_date])
+      new_changes << task unless task.changes.empty?
+
+      # Child task of current task & does not depend on any task
+      children = get_children(task, batch_tasks)
+      # Rails.logger.debug ">>> children: #{children.size}"
+
+      # Tasks depends on current task, currently apply to "Phase" task that
+      # positioned after the current task
+      dependents = get_dependents(task, batch_tasks)
+      # Rails.logger.debug ">>> dependents: #{dependents.size}"
+
+      # Find changed child tasks
+      if opt[:children]
+        new_changes += find_cascaded_changes(children,
+                                             batch_tasks,
+                                             task.start_date)
       end
-      task
+
+      # Find changed dependents tasks
+      if opt[:dependent]
+        new_changes += find_cascaded_changes(dependents,
+                                             batch_tasks,
+                                             task.end_date)
+      end
+      new_changes
+    end
+
+    def find_cascaded_changes(cascade_tasks, batch_tasks, start_date)
+      new_changes = []
+      cascade_tasks.each do |ref_task|
+        new_changes += find_changes(ref_task,
+                                    batch_tasks,
+                                    start_date: start_date)
+      end
+      new_changes
+    end
+
+    # Find all subtasks
+    def get_children(task, batch_tasks)
+      batch_tasks.select do |t|
+        t.parent_id &&
+          # Sub-tasks should have parent set to current task
+          t.parent_id.to_s == task.id.to_s
+      end
+    end
+
+    # Find all subtasks
+    def get_dependents(task, batch_tasks)
+      batch_tasks.select do |t|
+        t.depend_on &&
+          # Dependent tasks should have depends on set to current task
+          t.depend_on.to_s == task.id.to_s
+      end
+    end
+
+    # Find all siblings tasks
+    def get_siblings(task, batch_tasks)
+      result = batch_tasks.select do |t|
+        t.parent_id &&
+          # Sibling tasks should have same parent as current task
+          t.parent_id.to_s == task.parent_id.to_s &&
+          # Exclude current task since it might have updated value
+          # from current iteration
+          t.id.to_s != task.id.to_s
+      end
+      # Add back the updated version of current task
+      result << task
     end
 
     # Calculate end_date based on start_date + duration
@@ -116,48 +193,6 @@ module Cultivation
 
       duration = (end_date - start_date).to_i
       duration + 1
-    end
-
-    def find_changes(task, batch_tasks = [], opt = {})
-      Rails.logger.debug ">>> finding changes for #{task.name}"
-      opt = {children: true, dependent: true}.merge(opt) # default options
-
-      # Child task of current task & does not depend on any task
-      children = batch_tasks.select { |b| b.parent_id.to_s == task.id.to_s }
-      Rails.logger.debug ">>> children: #{children.size}"
-
-      # Tasks depends on current task
-      dependents = batch_tasks.select { |b| b.depend_on.to_s == task.id.to_s }
-      Rails.logger.debug ">>> dependents: #{dependents.size}"
-
-      new_changes = []
-      # Find changed child tasks
-      if !children.empty? && opt[:children]
-        new_changes += find_cascaded_changes(children,
-                                             batch_tasks,
-                                             task.start_date)
-      end
-
-      # Find changed dependents tasks
-      if !dependents.empty? && opt[:dependent]
-        new_changes += find_cascaded_changes(dependents,
-                                             batch_tasks,
-                                             task.end_date)
-      end
-      new_changes
-    end
-
-    def find_cascaded_changes(cascade_tasks, batch_tasks, start_date)
-      new_changes = []
-      cascade_tasks.each do |ref_task|
-        set_task_dates(ref_task, start_date)
-        new_changes << ref_task unless ref_task.changes.empty?
-        new_changes += find_changes(ref_task, batch_tasks)
-        Rails.logger.debug "# of new changes: #{new_changes.size}"
-        Rails.logger.debug "New Changes: #{new_changes.last&.name}"
-        Rails.logger.debug "Duration: #{new_changes.last&.duration}"
-      end
-      new_changes
     end
 
     def set_task_dates(ref_task, start_date)
@@ -199,13 +234,12 @@ module Cultivation
         #get all phases
         phases = batch.tasks.select { |b| b.is_phase == true }
         #find cure or dry phase
-        Rails.logger.debug "Task Min => #{min_date}"
-        Rails.logger.debug "Task Max => #{max_date}"
-
-        Rails.logger.debug "Batch Start Date => #{batch.start_date}"
+        # Rails.logger.debug "Task Min => #{min_date}"
+        # Rails.logger.debug "Task Max => #{max_date}"
+        # Rails.logger.debug "Batch Start Date => #{batch.start_date}"
 
         phase = phases.pluck(:phase).include?('cure') ? phases.detect { |b| b.phase == 'cure' } : phases.detect { |b| b.phase == 'dry' }
-        Rails.logger.debug "Phase End Date => #{phase.end_date if phase}"
+        # Rails.logger.debug "Phase End Date => #{phase.end_date if phase}"
 
         if max_date && phase && phase.end_date &&
            ((phase.end_date >= min_date && batch.start_date <= max_date) ||

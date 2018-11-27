dc5d7efd65cec60099f2944d32b1e18e02d7c057
Andy Yong <kwokhou@gmail.com>
2018-11-27 13:58:34 +0800

    Remove FIXME

--- app/commands/cultivation/create_task.rb
+++ app/commands/cultivation/create_task.rb
@@ -18,7 +18,7 @@ module Cultivation
       task_related = Cultivation::Task.find(args[:task_related_id])
       batch = task_related.batch
       argument = args.except(:task_related_id, :position)
-      # FIXME: This is only valid if "Add task below" TASK#439
+      # This is only valid if "Add task below" TASK#439
       argument[:parent_id] = if task_related.is_phase || task_related.is_category
                                task_related.id
                              else

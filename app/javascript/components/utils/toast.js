8a203f6e5ed858963faaed94e235805f099843b0
fathiabdulrahim <fathiabdrahim@yahoo.com>
2018-12-11 17:46:05 +0800

    update toast js

--- app/javascript/components/utils/toast.js
+++ app/javascript/components/utils/toast.js
@@ -1,17 +1,23 @@
-function fadeToast() {
+export const fadeToast = (message, type) => {
+
   let elm = $_('#toast')
   elm.classList.replace('fadeIn', 'fadeOut')
-  setTimeout(function() {
+  setTimeout(function () {
     $_('#toast').classList.remove('fadeOut', 'db')
   }, 1000)
+
 }
-function toast(message, type) {
+
+
+export const toast = (message, type) =>{
   let elm = $_('#toast')
   elm.innerHTML = message
   elm.classList.add('fadeIn', 'toast--' + type, 'db')
-  setTimeout(function() {
+  setTimeout(function () {
     fadeToast()
   }, 3000)
 }
 
-export { fadeToast, toast }
+
+
+

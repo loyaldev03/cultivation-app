cf200aff34674ca6cdb9488108e31c53df5eb193
fathiabdulrahim <fathiabdrahim@yahoo.com>
2018-12-13 11:29:58 +0800

    run formatter

--- app/javascript/components/utils/toast.js
+++ app/javascript/components/utils/toast.js
@@ -1,23 +1,16 @@
 export const fadeToast = (message, type) => {
-
   let elm = $_('#toast')
   elm.classList.replace('fadeIn', 'fadeOut')
-  setTimeout(function () {
+  setTimeout(function() {
     $_('#toast').classList.remove('fadeOut', 'db')
   }, 1000)
-
 }
 
-
-export const toast = (message, type) =>{
+export const toast = (message, type) => {
   let elm = $_('#toast')
   elm.innerHTML = message
   elm.classList.add('fadeIn', 'toast--' + type, 'db')
-  setTimeout(function () {
+  setTimeout(function() {
     fadeToast()
   }, 3000)
 }
-
-
-
-

27a93eae30099155cf3df6aefe1464d8a80f2d77
Andy Yong <kwokhou@gmail.com>
2019-01-07 14:22:59 +0800

    Make TaskNameField extends from InlineEditTextField

--- app/javascript/components/cultivation/tasks_setup/components/InlineEditTextField.js
+++ app/javascript/components/cultivation/tasks_setup/components/InlineEditTextField.js
@@ -29,6 +29,7 @@ export default class InlineEditTextField extends React.PureComponent {
   }
   render() {
     const { text, className, renderInput, min, type = 'text' } = this.props
+    const isAlignRight = type === 'number'
     return (
       <div className={`flex flex-auto items-center ${className}`}>
         {this.state.isEdit ? (
@@ -38,7 +39,9 @@ export default class InlineEditTextField extends React.PureComponent {
               min={min}
               autoFocus
               ref={input => (this.textInput = input)}
-              className="flex-auto b--grey link"
+              className={classNames('flex-auto b--grey link', {
+                tr: isAlignRight
+              })}
               onKeyPress={this.handleKeyPress}
               defaultValue={text}
             />
@@ -53,7 +56,9 @@ export default class InlineEditTextField extends React.PureComponent {
           <React.Fragment>
             <a
               href="#0"
-              className="link flex-auto h1"
+              className={classNames('link grey flex-auto h1', {
+                tr: isAlignRight
+              })}
               onClick={this.switchEditMode}
             >
               {text}

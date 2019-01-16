2988ce1f9ab7ec21b9baf8ec7f96c45617d78183
Karg Weng <karg@betacode.io>
2019-01-17 00:07:44 +0800

    Added attachment preview. WIP - task

--- app/javascript/components/issues/components/CommentMessage.js
+++ app/javascript/components/issues/components/CommentMessage.js
@@ -2,41 +2,119 @@ import React from 'react'
 import Avatar from '../../utils/Avatar.js'
 import { formatDate, formatTime } from '../../utils/DateHelper'
 
-const CommentMessage = ({
-  sender_first_name, 
-  sender_last_name, 
-  sender_photo, 
-  sender_id,
-  current_user_id, 
-  sent_at, 
-  message, 
-  quote= '',
-  resolved= false
-}) => {
+const Preview = ({url, preview, type, filename = ''}) => {
+  return (
+    <div
+      src="/"
+      mime_type={type}
+      style={{ width: 48, height: 48 }}
+      className="mr1 overflow-hidden relative hover-photo mb1"
+    >
+      <Thumbnail url={url} preview={preview} type={type} filename={filename} />
+      <div
+        className="zoom-btn"
+        style={{ width: 48, height: 48 }}
+        url={url}
+        /* onClick={() => this.onTogglePreview(x.url, x.mime_type)} */
+      >
+        <i className="material-icons absolute">search</i>
+      </div>
+    </div>
+  )
+}
 
+
+const Thumbnail = ({url, preview, type, filename}) => {
+  if (type.startsWith('image')) {
+    return (
+      <div
+        style={{
+          width: 48,
+          height: 48,
+          background: `url(${preview}) no-repeat center center`,
+          backgroundSize: 'cover'
+        }}
+      />
+    )
+  } else {
+    return (
+      <div
+        className="gray overflow-hidden f7 bg-white"
+        style={{
+          width: 48,
+          height: 48
+        }}
+      >
+        VID - {filename}
+      </div>
+    )
+  }
+}
+
+const CommentMessage = ({
+  sender_first_name,
+  sender_last_name,
+  sender_photo,
+  sender_id,
+  current_user_id,
+  sent_at,
+  message,
+  quote = '',
+  resolved = false,
+  attachments = [],
+}) => {
   const isMe = sender_id === current_user_id
   const align = isMe ? 'justify-end' : 'justify-start'
   return (
     <React.Fragment>
-      <div className={`pl4 pr3 mb3 flex ${align}`}>
+      <div className={`pl4 pr3 mb3 mt1 flex ${align}`}>
         <div className={`pt1 mr2 ${isMe && 'dn'}`}>
-          <Avatar firstName={sender_first_name} lastName={sender_last_name} size={25} />
+          <Avatar
+            firstName={sender_first_name}
+            lastName={sender_last_name}
+            size={25}
+            photoUrl={sender_photo}
+          />
         </div>
-        <div  style={{ minWidth: '40%' }}>
-          <div className="f6 dark-gray pv2 ph3 br2 bg-black-05 mb2">
-            {message}
+        <div style={{ minWidth: '40%', maxWidth: '85%' }}>
+          <div className="mb2 pv2 pl3 pr0 br2 bg-black-05">
+            <div className="flex">
+              <p className="f6 black-70 lh-title mt0 mb1 flex-auto">
+                {message}
+              </p>
+              <span className="material-icons black-05 hover-gray ph1 pointer" style={{ fontSize: '18px'}}>more_vert</span>
+            </div>
+            { 
+              attachments.length > 0 && (
+                <div className="flex flex-wrap mt2 mb1">
+                  { attachments.map((props) => <Preview key={props.url} {...props} />) }
+                </div>)
+            }
+            { 
+              // TODO: Refactor CommentMessage to TaskCreatedMessage so that 'quote' layout is not here.
+              quote.length > 0 && (
+                <div className="bg-white pa2 i f7 gray mt1 mr3">
+                  { quote }
+                </div>)
+            }
           </div>
-          
+
           <div className="fw4 gray" style={{ fontSize: '10px' }}>
-            <span className="orange">{sender_first_name} {sender_last_name}</span>, {formatDate(sent_at)} {formatTime(sent_at)}
+            <span className="orange">
+              {sender_first_name} {sender_last_name}
+            </span>
+            , {formatDate(sent_at)} {formatTime(sent_at)}
           </div>
-          
         </div>
         <div className={`pt1 ml2 ${!isMe && 'dn'}`}>
-          <Avatar firstName={sender_first_name} lastName={sender_last_name} size={25} />
+          <Avatar
+            firstName={sender_first_name}
+            lastName={sender_last_name}
+            size={25}
+            photoUrl={sender_photo}
+          />
         </div>
       </div>
-      
     </React.Fragment>
   )
 }
--- app/javascript/components/issues/components/Comments.js
+++ app/javascript/components/issues/components/Comments.js
@@ -2,18 +2,98 @@ import React from 'react'
 import Avatar from '../../utils/Avatar.js'
 import CommentMessage from './CommentMessage'
 class Comments extends React.Component {
-
   renderMessages() {
     const messages = [
-      { id: 1, sender_first_name: 'T', sender_last_name: 'K', sender_photo: null, sender_id: 1, current_user_id: 2, 
-        sent_at: new Date('01 Jan 1970 00:00:00 GMT'), message: 'Seems like we have an issue.', quote: '', resolved: false },
-      { id: 2, sender_first_name: 'S', sender_last_name: 'U', sender_photo: null, sender_id: 2, current_user_id: 2, 
-        sent_at: new Date('01 Jan 1970 00:00:00 GMT'), message: 'Yes it seems to be so...', quote: '', resolved: false },
-      { id: 3, sender_first_name: 'T', sender_last_name: 'K', sender_photo: null, sender_id: 1, current_user_id: 2, 
-        sent_at: new Date('01 Jan 1970 00:00:00 GMT'), message: 'Gonna call Mike.', quote: 'Yes it seems to be so...', resolved: false },
-      { id: 4, sender_first_name: 'T', sender_last_name: 'K', sender_photo: null, sender_id: 1, current_user_id: 2, 
-        sent_at: new Date('01 Jan 1970 00:00:00 GMT'), message: 'As you can see above, the stateless component is just a function. Thus, all the annoying and confusing quirks with Javascript’s this keyword are avoided. The entire component becomes easier to understand without the this keyword.', quote: 'Yes it seems to be so...', resolved: false },
-    ].map( x => <CommentMessage key={x.id} {...x} />)
+      {
+        id: 1,
+        sender_first_name: 'Tim',
+        sender_last_name: 'K',
+        sender_photo: null,
+        sender_id: 1,
+        current_user_id: 2,
+        sent_at: new Date('01 Jan 1970 00:00:00 GMT'),
+        message: 'Seems like we have an issue.',
+        quote: '',
+        resolved: false
+      },
+      {
+        id: 2,
+        sender_first_name: 'Sample',
+        sender_last_name: 'User',
+        sender_photo: null,
+        sender_id: 2,
+        current_user_id: 2,
+        sent_at: new Date('01 Jan 1970 00:00:00 GMT'),
+        message: 'Yes it seems to be so...',
+        quote: '',
+        resolved: false
+      },
+      {
+        id: 3,
+        sender_first_name: 'Tim',
+        sender_last_name: 'K',
+        sender_photo: null,
+        sender_id: 1,
+        current_user_id: 2,
+        sent_at: new Date('01 Jan 1970 00:00:00 GMT'),
+        message: 'Gonna call Mike.',
+        quote: 'Yes it seems to be so...',
+        resolved: false
+      },
+      {
+        id: 4,
+        sender_first_name: 'Tim',
+        sender_last_name: 'K',
+        sender_photo: null,
+        sender_id: 1,
+        current_user_id: 2,
+        sent_at: new Date('01 Jan 1970 00:00:00 GMT'),
+        message:
+          'As you can see above, the stateless component is just a function. Thus, all the annoying and confusing quirks with Javascript’s this keyword are avoided. The entire component becomes easier to understand without the this keyword.',
+        resolved: false
+      },
+      {
+        id: 5,
+        sender_first_name: 'Sample',
+        sender_last_name: 'User',
+        sender_photo: null,
+        sender_id: 2,
+        current_user_id: 2,
+        sent_at: new Date('01 Jan 1970 00:00:00 GMT'),
+        message:
+          'Captured some picture as ref.',
+        quote: 'Yes it seems to be so...',
+        attachments: [
+          { url: 'https://picsum.photos/300/200', preview: 'https://picsum.photos/300/200', type: 'image/png' },
+          { url: 'https://picsum.photos/300/201', preview: 'https://picsum.photos/300/201', type: 'image/png' },
+        ],
+        resolved: false
+      },
+      {
+        id: 6,
+        sender_first_name: 'Sample',
+        sender_last_name: 'User',
+        sender_photo: null,
+        sender_id: 2,
+        current_user_id: 2,
+        sent_at: new Date('01 Jan 1970 00:00:00 GMT'),
+        message:
+          'and some more...',
+        attachments: [
+          { url: 'https://picsum.photos/300/200', preview: 'https://picsum.photos/300/200', type: 'image/png' },
+          { url: 'https://picsum.photos/301/201', preview: 'https://picsum.photos/301/201', type: 'image/png' },
+          { url: 'https://picsum.photos/302/202', preview: 'https://picsum.photos/302/202', type: 'image/png' },
+          { url: 'https://picsum.photos/303/200', preview: 'https://picsum.photos/303/200', type: 'image/png' },
+          { url: 'https://picsum.photos/304/201', preview: 'https://picsum.photos/304/201', type: 'image/png' },
+          { url: 'https://picsum.photos/305/202', preview: 'https://picsum.photos/305/202', type: 'image/png' },
+          { url: 'https://picsum.photos/306/200', preview: 'https://picsum.photos/306/200', type: 'image/png' },
+          { url: 'https://picsum.photos/307/201', preview: 'https://picsum.photos/307/201', type: 'image/png' },
+          { url: 'https://picsum.photos/307/201', preview: 'https://picsum.photos/307/201', type: 'video/mp4', filename: 'axx111yy' },
+          { url: 'https://picsum.photos/308/202', preview: 'https://picsum.photos/308/202', type: 'image/png' },
+        ],
+        resolved: false
+      }
+    ].map(x => <CommentMessage key={x.id} {...x} />)
 
     return messages
   }
@@ -21,24 +101,43 @@ class Comments extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <div className="flex pl4 pr3 pb4 items-center mt3">
+        <div className="flex pl4 pr3 pb3 items-center mt3">
           <div className="f7 fw6 gray w-auto mr1">ISSUE #001</div>
           <div className="f7 fw6 gray w-auto mr1 self-start">&bull;</div>
           <div className="f7 fw6 gray w-auto">Discussion</div>
         </div>
-        { this.renderMessages() }
-        <div className="pl4 pr3 mb3 mt2">
+        {this.renderMessages()}
+        <div className="pl4 pr3 mv3">
           <div className="b--black-10 flex br3 ba w-100 ph2 pt1 pb2 flex items-center">
             <Avatar firstName="Sample" lastName="User" size={25} />
-            <textarea type="text" className="bn flex-auto flex ml1 f6 dark-grey outline-0" rows="1" />
+            <textarea
+              type="text"
+              className="bn flex-auto flex ml1 f6 dark-grey outline-0"
+              rows="1"
+            />
             <a href="#" className="ml1">
-              <span className="material-icons black-30 f6 v-mid" style={{ fontSize: '18px'}}>attach_file</span>
+              <span
+                className="material-icons black-30 f6 v-mid"
+                style={{ fontSize: '18px' }}
+              >
+                attach_file
+              </span>
             </a>
             <a href="#" className="ml1">
-              <span className="material-icons black-30 f6 v-mid" style={{ fontSize: '18px'}}>add</span>
+              <span
+                className="material-icons black-30 f6 v-mid"
+                style={{ fontSize: '18px' }}
+              >
+                add
+              </span>
             </a>
             <a href="#" className="ml1">
-              <span className="material-icons black-30 f6 v-mid" style={{ fontSize: '18px'}}>send</span>
+              <span
+                className="material-icons black-30 f6 v-mid"
+                style={{ fontSize: '18px' }}
+              >
+                send
+              </span>
             </a>
           </div>
         </div>

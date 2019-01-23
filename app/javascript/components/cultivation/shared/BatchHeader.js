d3d3cccc9ef2f9cceee257d3ad4c9a56dcdba934
Andy Yong <kwokhou@gmail.com>
2019-01-23 15:59:49 +0800

    Sanitize underscore text

--- app/javascript/components/cultivation/shared/BatchHeader.js
+++ app/javascript/components/cultivation/shared/BatchHeader.js
@@ -3,7 +3,8 @@ import {
   formatDate2,
   ActiveBadge,
   moneyFormatter,
-  decimalFormatter
+  decimalFormatter,
+  sanitizeText,
 } from '../../utils'
 
 class BatchHeader extends React.Component {
@@ -26,7 +27,6 @@ class BatchHeader extends React.Component {
       total_estimated_hour,
       estimated_harvest_date
     } = this.props
-    const batchSource = batch_source ? batch_source.replace(/_/g, ' ') : ''
     const batchQuantity = quantity ? quantity : 0
 
     return (
@@ -51,7 +51,7 @@ class BatchHeader extends React.Component {
                     </div>
                     <div className="w-40">
                       <div className="ttc">
-                        <label>{batchSource}</label>
+                        <label>{sanitizeText(batch_source)}</label>
                       </div>
                     </div>
                   </div>
@@ -97,8 +97,8 @@ class BatchHeader extends React.Component {
                       <label>Grow Method</label>
                     </div>
                     <div className="w-40">
-                      <div className="">
-                        <label>{grow_method}</label>
+                      <div className="ttc">
+                        <label>{sanitizeText(grow_method)}</label>
                       </div>
                     </div>
                   </div>
--- app/javascript/components/utils/ActiveBadge.js
+++ app/javascript/components/utils/ActiveBadge.js
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types'
 import classNames from 'classnames'
 import { BATCH_STATUS } from './'
 
-const ActiveBadge = React.memo(({ status, className }) => {
+const ActiveBadge = React.memo(({ status, className = '' }) => {
   return (
     <span
-      className={classNames(`f7 fw4 ph2 pv1 ba br2 dib w2 tc ${className}`, {
+      className={classNames(`f7 fw4 ph2 pv1 ba br2 dib tc ${className}`, {
         'grey b--grey': status === BATCH_STATUS.DRAFT,
         'bg-orange b--orange white': status === BATCH_STATUS.SCHEDULED,
         'bg-green b--green white': status === BATCH_STATUS.ACTIVE

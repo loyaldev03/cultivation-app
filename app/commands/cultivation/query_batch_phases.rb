f02d19472af848859253aeca6bfc2a400e07d0c7
Andy Yong <kwokhou@gmail.com>
2019-01-24 21:14:45 +0800

    QueryBatchPhase does'nt change phase date from db

--- app/commands/cultivation/query_batch_phases.rb
+++ app/commands/cultivation/query_batch_phases.rb
@@ -11,17 +11,15 @@ module Cultivation
       if @batch.present? && @batch.tasks.present?
         tasks = @batch.tasks.where(indent: 0)
         tasks = tasks.where(:phase.in => @phases) unless @phases.empty?
-        tasks = tasks.order_by(start_date: :asc)
+        tasks = tasks.order_by(position: :asc)
 
         phases = tasks.map do |task|
-          start_date = task.start_date.beginning_of_day
-          end_date = (task.end_date - 1.days).end_of_day
           OpenStruct.new(
             id: task.id,
             name: task.name,
             phase: task.phase,
-            start_date: start_date,
-            end_date: end_date,
+            start_date: task.start_date,
+            end_date: task.end_date,
             duration: task.duration,
           )
         end

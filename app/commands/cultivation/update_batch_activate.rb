656fa1042886b19b05d3656672df08ea2d62577e
fathiabdulrahim <fathiabdrahim@yahoo.com>
2019-01-24 16:25:06 +0800

    update validation

--- app/commands/cultivation/update_batch_activate.rb
+++ app/commands/cultivation/update_batch_activate.rb
@@ -16,6 +16,23 @@ module Cultivation
     def call
       errors.add(:batch_id, 'batch_id is required') if @batch_id.nil?
       errors.add(:start_date, 'Start Date is required') if @start_date.nil?
+
+      if errors.empty?
+        first_task = Cultivation::Task.find_by(
+          batch_id: @batch_id.to_bson_id,
+          position: 0,
+        )
+        first_task.start_date = @start_date
+        Rails.logger.debug "\033[31m first_task.start_date: #{first_task.start_date} \033[0m"
+        UpdateTask.call(@current_user, first_task, true)
+      end
+
+      validate
+    rescue StandardError
+      errors.add(:error, $!.message)
+    end
+
+    def validate
       batch = Cultivation::Batch.find(@batch_id)
       #validate purchase clone
       if batch.batch_source == 'clones_purchased'
@@ -29,18 +46,6 @@ module Cultivation
       #   errors.add(:batch_id, result.errors['strain']) unless result.success?
       # end
       #
-
-      if errors.empty?
-        first_task = Cultivation::Task.find_by(
-          batch_id: @batch_id.to_bson_id,
-          position: 0,
-        )
-        first_task.start_date = @start_date
-        Rails.logger.debug "\033[31m first_task.start_date: #{first_task.start_date} \033[0m"
-        UpdateTask.call(@current_user, first_task, true)
-      end
-    rescue StandardError
-      errors.add(:error, $!.message)
     end
   end
 end
--- app/commands/cultivation/validate_purchase_clone.rb
+++ app/commands/cultivation/validate_purchase_clone.rb
@@ -23,6 +23,7 @@ module Cultivation
       plant_task = tasks.detect { |a| a['indelible'] == 'plants' }
       #check purchase clone is selected or not
       if plant_task and plant_task.material_use.count == 0
+        #if no material is selected
         issue = Issues::Issue.find_by(
           task_id: plant_task.id,
           cultivation_batch_id: batch.id.to_s,
@@ -44,12 +45,53 @@ module Cultivation
             )
         end
         errors.add('strain', 'Purchase Clone is not selected')
-      end
-      #compare for all batches that doesnt started yet
+      else
 
-      #create issue clone is not selected
-      #check if products > quantity
+        #make command to sum total booked per product id
+        batches_selected = Cultivation::Batch.where(:start_date.gte => Time.now).not_in(id: batch.id)
+
+        # batches_selected = batches.select{|a| a[:start_date] > Time.now}
+        #add to filter by status active and scheduled batch
+        purchased_clone_products = plant_task.material_use.pluck(:product_id)
+        material_available = []
+        plant_task.material_use.each do |material|
+          product = material.product
+          material_available << {product_id: product.id, quantity: product.packages.sum { |a| a.quantity }.to_i}
+        end
+
+        material_booked = []
+        plant_tasks = Cultivation::Task.where(:batch_id.in => batches_selected.pluck(:id), indelible: 'plants')
+        Rails.logger.debug "Plant Task Details => #{plant_tasks.first.id.to_s}"
+        Rails.logger.debug "Plant Task ==> #{plant_tasks.first.material_use.count}"
+        plant_tasks.each do |task|
+          task.material_use.each do |material|
+            Rails.logger.debug 'SINI LAH KIRA'
+            product = material.product
+            material_booked << {product_id: product.id, quantity: product.packages.sum { |a| a.quantity }.to_i}
+          end
+        end
+        Rails.logger.debug "Material Available ==> #{material_available.inspect}"
+        Rails.logger.debug "Material Booked ==> #{material_booked.inspect}"
+        material_available.each do |material|
+          book_material = material_booked.detect { |a| a[:product_id] == material[:product_id] }
+          material_quantity = 0
+          material_quantity = book_material[:quantity] if book_material
+          Rails.logger.debug "Book Material ==> #{book_material.present?}"
+          Rails.logger.debug "Sum #{material[:quantity]} - #{material_quantity}"
+          Rails.logger.debug "Sum #{material[:quantity] - material_quantity}"
+        end
+        # batches_selected.each do |batch|
+        #   plant_task = batch.tasks.detect { |a| a['indelible'] == 'plants' }
+
+        #   material_booked << {}
+        # end
+
+        #compare for all batches that doesnt started yet
+        #create issue if not sufficient
+        #check if products > quantity
+      end
     rescue
+      Rails.logger.debug "#{$!.message}"
       errors.add(:error, $!.message)
     end
   end

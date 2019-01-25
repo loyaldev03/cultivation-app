a3c3b3a19d40d77e554ddbc0ac8a50f51441ffaf
Andy Yong <kwokhou@gmail.com>
2019-01-25 12:15:00 +0800

    Combine Tray if duplicate entry from UI

--- app/commands/cultivation/save_tray_plans.rb
+++ app/commands/cultivation/save_tray_plans.rb
@@ -2,10 +2,8 @@ module Cultivation
   class SaveTrayPlans
     prepend SimpleCommand
 
-    attr_reader :batch_id, :locations
-
     def initialize(batch_id, plans, quantity)
-      @batch_id = batch_id
+      @batch_id = batch_id&.to_bson_id
       @plans = plans
       @quantity = quantity
     end
@@ -14,7 +12,7 @@ module Cultivation
       if @batch_id.present? && @plans.any?
         # Rails.logger.debug "\033[34m Find Batch: #{@batch_id} \033[0m"
         # Rails.logger.debug "\033[34m # of plans: #{@plans.length} \033[0m"
-        batch = Cultivation::Batch.find_by(id: @batch_id.to_bson_id)
+        batch = Cultivation::Batch.find_by(id: @batch_id)
         if batch.present?
           facility = Facility.find_by(id: batch.facility_id)
           # Save selected mother plants
@@ -35,12 +33,12 @@ module Cultivation
             # New location plans
             phase_plan = phase_trays.detect { |p| p[:phase] == phase_info.phase }
             # Rails.logger.debug "\033[34m trays for phase: \033[0m"
-            locations = phase_plan[:trays]
+            trays = phase_plan[:trays]
             # Build new booking record of trays
             new_plans += build_tray_plans(batch.facility_id,
                                           batch.id,
                                           phase_info,
-                                          locations)
+                                          trays)
           end
           batch.save!
           # Save all tray plans
@@ -99,26 +97,32 @@ module Cultivation
       tray_plans
     end
 
-    def build_tray_plans(facility_id, batch_id, phase_info, locations = [])
+    def build_tray_plans(facility_id, batch_id, phase_info, trays = [])
       current_time = Time.zone.now
-      # Important: Must save capacity as integer for
-      # QueryAvailableTrays commands to work.
-      locations.map do |loc|
-        {
-          facility_id: facility_id.to_bson_id,
-          batch_id: batch_id.to_bson_id,
-          room_id: loc[:room_id].to_bson_id,
-          row_id: loc[:row_id].to_bson_id,
-          shelf_id: loc[:shelf_id].to_bson_id,
-          tray_id: loc[:tray_id].to_bson_id,
-          start_date: phase_info.start_date,
-          end_date: phase_info.end_date,
-          capacity: loc[:tray_capacity].to_i,
-          phase: phase_info.phase,
-          c_at: current_time,
-          u_at: current_time,
-        }
+      plans = []
+      trays.each do |tray|
+        # Combine booked quantity for same Tray
+        existing = plans.detect { |x| x[:tray_id] == tray[:tray_id].to_bson_id }
+        if existing.present?
+          existing[:capacity] += tray[:tray_capacity].to_i
+        else
+          plans << {
+            facility_id: facility_id.to_bson_id,
+            batch_id: batch_id.to_bson_id,
+            room_id: tray[:room_id].to_bson_id,
+            row_id: tray[:row_id].to_bson_id,
+            shelf_id: tray[:shelf_id].to_bson_id,
+            tray_id: tray[:tray_id].to_bson_id,
+            start_date: phase_info.start_date,
+            end_date: phase_info.end_date,
+            capacity: tray[:tray_capacity].to_i, # Important: Must as integer for
+            phase: phase_info.phase,
+            c_at: current_time,
+            u_at: current_time,
+          }
+        end
       end
+      plans
     end
   end
 end

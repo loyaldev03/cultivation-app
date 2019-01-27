6042d3df9357d45199ea768bdc3a2dd4bd6132e9
fathiabdulrahim <fathiabdrahim@yahoo.com>
2019-01-27 22:15:38 +0800

    update validate purchase clone

--- app/commands/cultivation/validate_purchase_clone.rb
+++ app/commands/cultivation/validate_purchase_clone.rb
@@ -52,33 +52,60 @@ module Cultivation
 
         # batches_selected = batches.select{|a| a[:start_date] > Time.now}
         #add to filter by status active and scheduled batch
-        purchased_clone_products = plant_task.material_use.pluck(:product_id)
+        purchased_clone_products = plant_task.material_use
         material_available = []
-        plant_task.material_use.each do |material|
+        purchased_clone_products.each do |material|
           product = material.product
           material_available << {product_id: product.id, quantity: product.packages.sum { |a| a.quantity }.to_i}
         end
 
         material_booked = []
         plant_tasks = Cultivation::Task.where(:batch_id.in => batches_selected.pluck(:id), indelible: 'plants')
-        Rails.logger.debug "Plant Task Details => #{plant_tasks.first.id.to_s}"
-        Rails.logger.debug "Plant Task ==> #{plant_tasks.first.material_use.count}"
+
         plant_tasks.each do |task|
           task.material_use.each do |material|
-            Rails.logger.debug 'SINI LAH KIRA'
             product = material.product
-            material_booked << {product_id: product.id, quantity: product.packages.sum { |a| a.quantity }.to_i}
+            material_booked << {product_id: product.id, quantity: material.quantity}
           end
         end
-        Rails.logger.debug "Material Available ==> #{material_available.inspect}"
-        Rails.logger.debug "Material Booked ==> #{material_booked.inspect}"
+
         material_available.each do |material|
           book_material = material_booked.detect { |a| a[:product_id] == material[:product_id] }
           material_quantity = 0
-          material_quantity = book_material[:quantity] if book_material
-          Rails.logger.debug "Book Material ==> #{book_material.present?}"
-          Rails.logger.debug "Sum #{material[:quantity]} - #{material_quantity}"
-          Rails.logger.debug "Sum #{material[:quantity] - material_quantity}"
+          material_quantity = book_material[:quantity] if book_material.present?
+
+          remaining_material = material[:quantity] - material_quantity.to_i
+          product_needed = purchased_clone_products.detect { |a| a[:product_id] == material[:product_id] }
+
+          unless remaining_material >= product_needed.quantity
+            Rails.logger.debug 'InSufficient'
+            issue = Issues::Issue.find_by(
+              task_id: plant_task.id,
+              cultivation_batch_id: batch.id.to_s,
+              reported_by: args[:current_user].id,
+              title: "Insufficient Purchase Clone #{product_needed&.product&.name}",
+            )
+            #create issue
+            unless issue and issue.persisted?
+              issue = Issues::Issue
+                .create!(
+                  issue_no: Issues::Issue.count + 1,
+                  title: "Insufficient Purchase Clone #{product_needed&.product&.name}",
+                  description: "Insufficient Purchase Clone #{product_needed&.product&.name}",
+                  severity: 'severe',
+                  issue_type: 'task_from_batch',
+                  status: 'open',
+                  task_id: plant_task.id,
+                  cultivation_batch_id: batch.id.to_s,
+                  reported_by: args[:current_user].id,
+                )
+            end
+
+            errors.add('strain', "Insufficient Purchase Clone #{product_needed&.product&.name}")
+          end
+          #CONTINUE FROM HERE
+          #check remaining material sufficient or not
+          #if not create an issue and return error not enough
         end
         # batches_selected.each do |batch|
         #   plant_task = batch.tasks.detect { |a| a['indelible'] == 'plants' }

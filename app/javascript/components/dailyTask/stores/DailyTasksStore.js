a983da42f6bdb292f2eecc04cdb60ebed8fd7aac
Andy Yong <kwokhou@gmail.com>
2019-02-22 17:10:24 +0800

    Added updateNote action

--- app/javascript/components/dailyTask/stores/DailyTasksStore.js
+++ app/javascript/components/dailyTask/stores/DailyTasksStore.js
@@ -7,6 +7,26 @@ class DailyTaskStore {
     this.batches.replace(batches)
   }
 
+
+  @action
+  updateNote(note) {
+    this.batches.forEach(b => {
+      const task = b.tasks.find(x => x.id === note.task_id)
+      if (task) {
+        if (task.notes) {
+          const exist = task.notes.find(n => n.id === note.id)
+          if (exist) {
+            task.notes = task.notes.map(n => (n.id === note.id ? note : n))
+          } else {
+            task.notes = [...task.notes, note]
+          }
+        } else {
+          task.notes = [note]
+        }
+      }
+    })
+  }
+
   @computed
   get bindable() {
     return this.batches.slice()

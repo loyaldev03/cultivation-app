9e6c1e57bc11b5f30000c564b301dcd6e2722551
Andy Yong <kwokhou@gmail.com>
2019-02-28 16:40:55 +0800

    Update API to save add_nutrients

--- app/commands/cultivation/save_material_use.rb
+++ app/commands/cultivation/save_material_use.rb
@@ -2,11 +2,13 @@ module Cultivation
   class SaveMaterialUse
     prepend SimpleCommand
 
-    attr_reader :args
+    attr_reader :current_user, :id, :items, :nutrients
 
-    def initialize(id, items)
-      @id = id
+    def initialize(current_user, id, items, nutrients)
+      @current_user = current_user
+      @id = id.to_bson_id
       @items = items
+      @nutrients = nutrients
     end
 
     def call
@@ -16,10 +18,22 @@ module Cultivation
     private
 
     def save_record
-      record = Cultivation::Task.find(@id)
+      record = Cultivation::Task.find(id)
       record.material_use = []
-      @items.each do |item|
-        record.material_use.build({product_id: item[:product_id], quantity: item[:quantity], uom: item[:uom]})
+      record.add_nutrients = []
+      items.each do |item|
+        record.material_use.build(
+          product_id: item[:product_id],
+          quantity: item[:quantity],
+          uom: item[:uom],
+        )
+      end
+      nutrients.each do |nutrient|
+        value = nutrient[:value].present? ? nutrient[:value].to_f : 0.00
+        record.add_nutrients.build(
+          element: nutrient[:element],
+          value: value,
+        )
       end
       record.save!
       record

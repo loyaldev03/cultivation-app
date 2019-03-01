7f36fadd4e366da0d96c78881759c559e240b124
fathiabdulrahim <fathiabdrahim@yahoo.com>
2019-03-01 11:29:55 +0800

    update nutrientEditor style

--- app/javascript/components/utils/FormHelpers.js
+++ app/javascript/components/utils/FormHelpers.js
@@ -43,7 +43,7 @@ function NumericInput({
       {label && <label className="f6 fw6 db mb1 gray ttc">{label}</label>}
       <input
         value={value}
-        className="db w-100 pa2 f6 black ba b--black-20 br2 outline-0 no-spinner"
+        className="db w-100 pa2 f6 black ba b--black-20 br2 outline-0 no-spinner tr"
         type="number"
         placeholder={placeholder}
         onChange={onChange}

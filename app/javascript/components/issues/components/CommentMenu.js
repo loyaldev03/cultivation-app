9d580f4b5d32fddab95450eaf600e5a7ff532e09
Mr.Worldwide <syahfisal37@yahoo.com>
2019-03-06 16:58:28 +0800

    Comments popper to tippy blocker

--- app/javascript/components/issues/components/CommentMenu.js
+++ app/javascript/components/issues/components/CommentMenu.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Manager, Reference, Popper } from 'react-popper'
+import Tippy from '@tippy.js/react';
 
 const MenuButton = ({ icon, text, onClick, className = '' }) => {
   return (
@@ -25,55 +26,91 @@ const CommentMenu = ({
   handleDelete
 }) => {
   return (
-    <Manager>
-      <Reference>
-        {({ ref }) => {
-          return (
-            <span
+    <Tippy
+    placement="bottom-end"
+    trigger="click"
+    content={<div
+                className="bg-white f6 flex grey"
+              >
+                <div className="db shadow-4" onMouseLeave={handleMouseLeave}>
+                  <MenuButton icon="reply" text="Reply" onClick={handleReply} />
+                  <MenuButton icon="playlist_add" text="Convert to task" />
+                  <MenuButton
+                    icon="check"
+                    text="Resolve"
+                    onClick={handleResolve}
+                  />
+                  {isMe && (
+                    <MenuButton icon="edit" text="Edit" onClick={handleEdit} />
+                  )}
+                  {isMe && (
+                    <MenuButton
+                      icon="delete"
+                      text="Delete"
+                      onClick={handleDelete}
+                    />
+                  )}
+                </div>
+              </div>}
+    >
+         <span
               className="material-icons black-10 hover-dark-gray ph1 pointer"
               style={{ fontSize: '18px', height: '21px' }}
-              ref={ref}
+            
               onClick={() => handleEllipsisClick(id)}
             >
               more_vert
             </span>
-          )
-        }}
-      </Reference>
-      {isOpen && (
-        <Popper placement="bottom-start">
-          {({ ref, style, placement, arrowProps }) => (
-            <div
-              ref={ref}
-              style={style}
-              data-placement={placement}
-              className="bg-white f6 flex"
-            >
-              <div className="db shadow-4" onMouseLeave={handleMouseLeave}>
-                <MenuButton icon="reply" text="Reply" onClick={handleReply} />
-                <MenuButton icon="playlist_add" text="Convert to task" />
-                <MenuButton
-                  icon="check"
-                  text="Resolve"
-                  onClick={handleResolve}
-                />
-                {isMe && (
-                  <MenuButton icon="edit" text="Edit" onClick={handleEdit} />
-                )}
-                {isMe && (
-                  <MenuButton
-                    icon="delete"
-                    text="Delete"
-                    onClick={handleDelete}
-                  />
-                )}
-              </div>
-              <div ref={arrowProps.ref} style={arrowProps.style} />
-            </div>
-          )}
-        </Popper>
-      )}
-    </Manager>
+    </Tippy>
+    // <Manager>
+    //   <Reference>
+    //     {({ ref }) => {
+    //       return (
+    //         <span
+    //           className="material-icons black-10 hover-dark-gray ph1 pointer"
+    //           style={{ fontSize: '18px', height: '21px' }}
+    //           ref={ref}
+    //           onClick={() => handleEllipsisClick(id)}
+    //         >
+    //           more_vert
+    //         </span>
+    //       )
+    //     }}
+    //   </Reference>
+    //   {isOpen && (
+    //     <Popper placement="bottom-start">
+    //       {({ ref, style, placement, arrowProps }) => (
+    //         <div
+    //           ref={ref}
+    //           style={style}
+    //           data-placement={placement}
+    //           className="bg-white f6 flex"
+    //         >
+    //           <div className="db shadow-4" onMouseLeave={handleMouseLeave}>
+    //             <MenuButton icon="reply" text="Reply" onClick={handleReply} />
+    //             <MenuButton icon="playlist_add" text="Convert to task" />
+    //             <MenuButton
+    //               icon="check"
+    //               text="Resolve"
+    //               onClick={handleResolve}
+    //             />
+    //             {isMe && (
+    //               <MenuButton icon="edit" text="Edit" onClick={handleEdit} />
+    //             )}
+    //             {isMe && (
+    //               <MenuButton
+    //                 icon="delete"
+    //                 text="Delete"
+    //                 onClick={handleDelete}
+    //               />
+    //             )}
+    //           </div>
+    //           <div ref={arrowProps.ref} style={arrowProps.style} />
+    //         </div>
+    //       )}
+    //     </Popper>
+    //   )}
+    // </Manager>
   )
 }
 

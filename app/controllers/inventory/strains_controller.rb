ba7d5cd44d989e92d7ebc020aa34abd32c40abc6
fathiabdulrahim <fathiabdrahim@yahoo.com>
2019-03-21 14:07:48 +0800

    added facility_id to the query

--- app/controllers/inventory/strains_controller.rb
+++ app/controllers/inventory/strains_controller.rb
@@ -2,7 +2,7 @@ module Inventory
   class StrainsController < ApplicationController
     def index
       @facilities = Facility.completed
-      @locations = QueryAllValidFacilityLocations.call.result
+      @locations = QueryAllValidFacilityLocations.call(facility_id: params[:facility_id]).result
     end
   end
 end

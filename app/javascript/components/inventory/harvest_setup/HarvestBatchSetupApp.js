5873b6498427d6502222b3d122a1d2ec7340730a
fathiabdulrahim <fathiabdrahim@yahoo.com>
2019-03-22 11:00:47 +0800

    fix location selector using async

--- app/javascript/components/inventory/harvest_setup/HarvestBatchSetupApp.js
+++ app/javascript/components/inventory/harvest_setup/HarvestBatchSetupApp.js
@@ -1,3 +1,4 @@
+import 'babel-polyfill'
 import React from 'react'
 import { observer } from 'mobx-react'
 import ReactTable from 'react-table'
--- app/javascript/components/inventory/simple_cultivation_setup/SimpleCultivationBatchSetupApp.js
+++ app/javascript/components/inventory/simple_cultivation_setup/SimpleCultivationBatchSetupApp.js
@@ -1,3 +1,4 @@
+import 'babel-polyfill'
 import React from 'react'
 import { toJS } from 'mobx'
 import { observer } from 'mobx-react'

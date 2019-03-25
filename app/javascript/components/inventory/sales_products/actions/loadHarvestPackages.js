5cd4e924d4644b628b3df26e207e23af0948acf2
fathiabdulrahim <fathiabdrahim@yahoo.com>
2019-03-25 12:41:30 +0800

    run formatter

--- app/javascript/components/inventory/sales_products/actions/loadHarvestPackages.js
+++ app/javascript/components/inventory/sales_products/actions/loadHarvestPackages.js
@@ -1,8 +1,11 @@
 import { httpGetOptions } from '../../../utils'
 import harvestPackageStore from '../store/HarvestPackageStore'
 
-const loadHarvestPackages = (facility_id) => {
-  return fetch(`/api/v1/sales_products/harvest_packages?facility_id=${facility_id}`, httpGetOptions)
+const loadHarvestPackages = facility_id => {
+  return fetch(
+    `/api/v1/sales_products/harvest_packages?facility_id=${facility_id}`,
+    httpGetOptions
+  )
     .then(response => {
       return response.json().then(data => ({
         status: response.status,

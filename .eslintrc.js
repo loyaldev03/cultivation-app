2b00512899a5a3563f1243d3a05840cf6232ea15
Andy Yong <kwokhou@gmail.com>
2019-03-26 12:24:34 +0800

    Revert eslint unused var rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -40,7 +40,9 @@ module.exports = {
             "error",
             "never"
         ],
-        "no-unused-vars": 1,
+        "no-unused-vars": [
+          "error", { "vars": "all", "args": "after-used", "ignoreRestSiblings": true }
+        ],
         "react/jsx-uses-vars": 1,
     }
 };

8466ee465c234a928715d913d078b6df0cf7b800
fathiabdulrahim <fathiabdrahim@yahoo.com>
2019-03-27 13:43:27 +0800

    run formatter

--- app/javascript/components/dailyTask/components/ClipPotTagForm.js
+++ app/javascript/components/dailyTask/components/ClipPotTagForm.js
@@ -33,7 +33,9 @@ class ClipPotTagForm extends React.Component {
               Create plant ID after clipping
             </h1>
             <div className="flex justify-end items-center pt3">
-              <i className="material-icons orange pointer pa1 mh2">error_outline</i>
+              <i className="material-icons orange pointer pa1 mh2">
+                error_outline
+              </i>
               <a href="#0" className="btn btn--secondary btn--small">
                 Add Issue
               </a>
@@ -97,7 +99,11 @@ const MotherPlantRow = forwardRef(
       }
     }
     const onScanClipping = e => {
-      if (e.key === 'Enter' && e.target.value && !plants.includes(e.target.value)) {
+      if (
+        e.key === 'Enter' &&
+        e.target.value &&
+        !plants.includes(e.target.value)
+      ) {
         setPlants([...plants, e.target.value])
       }
     }

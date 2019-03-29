3064dcc8a84e6dd785eb8219573cf48f7c315549
fathiabdulrahim <fathiabdrahim@yahoo.com>
2019-03-29 11:59:04 +0800

    update convert product editor barcode scanner

--- app/javascript/components/utils/InputBarcode.js
+++ app/javascript/components/utils/InputBarcode.js
@@ -75,15 +75,16 @@ const InputBarcode = forwardRef(
             </React.Fragment>
           )}
         </div>
-        {multiple ?
+        {multiple ? (
           <div className="w-100 mb2 mt2 flex justify-end">
-            <a className="ph2 pv2 btn--secondary f6 link pointer" onClick={onShowScanner}>
+            <a
+              className="ph2 pv2 btn--secondary f6 link pointer"
+              onClick={onShowScanner}
+            >
               Scan Tags
             </a>
           </div>
-          : 
-          null  
-        }
+        ) : null}
         <div className="flex items-center mt3">
           <div className="scanner" ref={x => (scannerRef = x)} />
         </div>

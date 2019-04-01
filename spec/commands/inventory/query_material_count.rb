3a1956e603c0a76bf7473ae0ce46accc2432de19
Karg Weng <karg@betacode.io>
2019-04-02 01:02:06 +0800

    Material used wip query

---
+++ spec/commands/inventory/query_material_count.rb
@@ -0,0 +1,9 @@
+# Test 1 - should pass when inventory count is more than zero & zero planned
+
+# Test 2 - should pass when inventory count > inventory planned
+
+# Test 3 - should pass when inventory count > (inventory planned + inventory used + inventory waste)
+
+# Test 4 - should return the value that is higher between planned and used + waste
+
+# Test 5 -

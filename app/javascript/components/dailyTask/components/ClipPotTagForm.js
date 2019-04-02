0f2cfe8d2a3889b9d5f275242cd7bdb007fda3a9
Andy Yong <kwokhou@gmail.com>
2019-04-02 16:10:03 +0800

    Moved PlantTagList to external file for sharing

--- app/javascript/components/dailyTask/components/ClipPotTagForm.js
+++ app/javascript/components/dailyTask/components/ClipPotTagForm.js
@@ -3,6 +3,7 @@ import isEmpty from 'lodash.isempty'
 import { observer } from 'mobx-react'
 import SidebarStore from '../stores/SidebarStore'
 import ClippingStore from '../stores/ClippingStore'
+import PlantTagList from './PlantTagList'
 import { AdjustmentMessage, InputBarcode, SlidePanelHeader } from '../../utils'
 
 @observer
@@ -258,28 +259,5 @@ const MotherPlantRow = forwardRef(
     )
   }
 )
-
-const PlantTagList = ({ onDelete, plantTags = [] }) => {
-  if (isEmpty(plantTags)) {
-    return <p className="mv1 i light-grey">Nothing yet.</p>
-  }
-  return (
-    <ol className="clippings">
-      {plantTags.map(tag => (
-        <li key={tag} className="clippings__item hide-child">
-          <span className="flex items-center">
-            {tag}
-            <i
-              className="ml2 material-icons md-16 child pointer"
-              onClick={() => onDelete(tag)}
-            >
-              delete_outline
-            </i>
-          </span>
-        </li>
-      ))}
-    </ol>
-  )
-}
 
 export default ClipPotTagForm

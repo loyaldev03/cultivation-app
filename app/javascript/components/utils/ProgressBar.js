0cee5a63792892c8d3ef85cb35659dde58a690ad
Andy Yong <kwokhou@gmail.com>
2019-04-04 13:55:36 +0800

    Added ProgresBar component

---
+++ app/javascript/components/utils/ProgressBar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const ProgressBar = ({
+  height = 10,
+  percent = 0,
+  className = '',
+  show = true
+}) => {
+  if (!show) return null
+  const style = {
+    width: `${percent}%`,
+    height: height
+  }
+  return (
+    <div
+      className={`bg-moon-gray br-pill overflow-y-hidden ${className}`}
+      style={{ height: height }}
+    >
+      <div className={'bg-orange br-pill shadow-1'} style={style} />
+    </div>
+  )
+}
+
+export { ProgressBar }

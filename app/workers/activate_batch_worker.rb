b934e279be83d43a9ae727bb666796dd95550219
Andy Yong <kwokhou@gmail.com>
2019-04-11 11:24:46 +0800

    Faster worker interval in development env

--- app/workers/activate_batch_worker.rb
+++ app/workers/activate_batch_worker.rb
@@ -2,6 +2,8 @@ class ActivateBatchWorker
   include Sidekiq::Worker
 
   def perform(*args)
+    @logger = Logger.new(STDOUT)
+    @logger.debug "Execute Cultivation::ActivateBatch @ #{Time.current}"
     Cultivation::ActivateBatch.call(Time.current)
   end
 end

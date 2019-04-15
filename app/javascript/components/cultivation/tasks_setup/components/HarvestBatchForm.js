4939231e84af9a9a61f75b32739a9300ed917da4
Karg Weng <karg@betacode.io>
2019-04-15 15:15:58 +0800

    wip

--- app/javascript/components/cultivation/tasks_setup/components/HarvestBatchForm.js
+++ app/javascript/components/cultivation/tasks_setup/components/HarvestBatchForm.js
@@ -3,14 +3,15 @@ import PropTypes from 'prop-types'
 import { observer } from 'mobx-react'
 import Select from 'react-select'
 import { TextInput, NumericInput } from '../../../utils/FormHelpers'
-import { SlidePanelHeader, SlidePanelFooter } from '../../../utils'
+import { SlidePanelHeader, SlidePanelFooter, LocationPicker } from '../../../utils'
+
 import reactSelectStyle from '../../../utils/reactSelectStyle'
 
 // @observer
 class HarvestBatchForm extends React.Component {
   state = {
     name: '',
-    harvest_room: '',
+    locationId: '',
     uom: null,
     errors: {}
   }
@@ -21,14 +22,18 @@ class HarvestBatchForm extends React.Component {
 
   validate = () => {}
 
-  handleChangeText = fieldName => e => {
+  onValueChanged = fieldName => e => {
     this.setState({
       [fieldName]: e.target.value
     })
   }
 
-  handleUomChanged = uom => {
+  onUomChanged = uom => {
     this.setState({ uom })
+  }
+
+  onLocationChanged = item => {
+    console.log(item)
   }
 
   render() {
@@ -44,7 +49,7 @@ class HarvestBatchForm extends React.Component {
             <TextInput
               label={'Harvest batch name'}
               value={name}
-              onChange={this.handleChangeText('name')}
+              onChange={this.onValueChanged('name')}
               errors={errors}
               errorField="name"
             />
@@ -56,19 +61,19 @@ class HarvestBatchForm extends React.Component {
             <Select
               options={uomOptions}
               value={uom}
-              onChange={this.handleUomChanged}
+              onChange={this.onUomChanged}
               styles={reactSelectStyle}
             />
           </div>
         </div>
+        
         <div className="ph4 mt3 mb4 flex">
           <div className="w-100">
-            <TextInput
-              label={'Harvest room'}
-              value={harvest_room}
-              onChange={this.handleChangeText('harvest_room')}
-              errors={errors}
-              errorField="harvest_room"
+            <LocationPicker 
+              purpose="harvest"
+              facility_id={this.props.facilityId}
+              location_id={this.state.locationId}
+              onChange={this.onLocationChanged}
             />
           </div>
         </div>

b03af5d16b187874d222d28b8279edf2a7470a62
Andy Yong <kwokhou@gmail.com>
2019-04-15 13:36:43 +0800

    Minor refactoring to done_task codes

--- app/commands/daily_task/done_task.rb
+++ app/commands/daily_task/done_task.rb
@@ -2,32 +2,45 @@ module DailyTask
   class DoneTask
     prepend SimpleCommand
 
-    def initialize(user_id, task_id, args = {})
-      @task = Cultivation::Task.find(task_id)
-      @user = User.find(user_id)
-      @args = args
+    attr_reader :task_id, :user_id
+
+    def initialize(user_id, task_id)
+      @task_id = task_id
+      @user_id = user_id
     end
 
     def call
-      if validate?
-        last_time_log = @task.time_logs.find_by(end_time: nil)
-        last_time_log.stop! if last_time_log
-        @task.update(work_status: 'done')
-        CalculateTotalActualCostJob.perform_now(@task.id.to_s)
-        MovePlantsToNextPhaseJob.perform_later(@task.batch_id.to_s)
-        @task
+      if valid_params?
+        last_time_log = task.time_logs.find_by(end_time: nil)
+        last_time_log&.stop!
+        task.update(work_status: 'done')
+        CalculateTotalActualCostJob.perform_now(task.id.to_s)
+        MovePlantsToNextPhaseJob.perform_later(task.batch_id.to_s)
+        task
       end
     rescue StandardError
       errors.add(:error, $!.message)
     end
 
-    def validate?
-      @task.present? #and status_include?
+    private
+
+    def task
+      @task ||= Cultivation::Task.find(task_id)
     end
 
-    def status_include?
-      status_allowed = ['started']
-      status_allowed.include?(@work_day.aasm_state)
+    def user
+      @user ||= User.find(user_id)
+    end
+
+    def valid_params?
+      if task_id.nil?
+        errors.add(:task_id, 'task_id is required')
+      end
+      if user_id.nil?
+        errors.add(:user_id, 'user_id is required')
+      end
+      errors.empty?
     end
   end
 end
+

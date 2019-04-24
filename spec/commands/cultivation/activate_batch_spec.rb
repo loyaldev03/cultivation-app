4d36bc1d41eb04d3238f1c5c2b29724b51c71ce9
Andy Yong <kwokhou@gmail.com>
2019-04-24 16:15:58 +0800

    Use time travel to validate batch stages

--- spec/commands/cultivation/activate_batch_spec.rb
+++ spec/commands/cultivation/activate_batch_spec.rb
@@ -42,7 +42,7 @@ RSpec.describe Cultivation::ActivateBatch, type: :command do
   let(:current_user) { create(:user, facilities: [facility.id]) }
   let!(:batch1) do
     Time.zone = facility.timezone
-    start_date = Time.zone.now.beginning_of_day
+    start_date = Time.zone.local(2018, 2, 1, 8, 30, 0)
     create(:batch, :scheduled,
            facility_strain: facility_strain,
            facility: facility,
@@ -51,87 +51,127 @@ RSpec.describe Cultivation::ActivateBatch, type: :command do
            current_growth_stage: Constants::CONST_CLONE,
            batch_source: Constants::PURCHASED_CLONES_KEY)
   end
-
-  let!(:task1) do
-    create(:task,
-           indelible: Constants::INDELIBLE_GROUP,
-           indent: 0,
-           batch: batch1,
-           start_date: batch1.start_date,
-           phase: Constants::CONST_CLONE)
-    create(:task,
-           indelible: Constants::INDELIBLE_CLIP_POT_TAG,
-           batch: batch1,
-           start_date: batch1.start_date,
-           phase: Constants::CONST_CLONE)
+  let!(:task_staying_clone) do
+    start_date = batch1.start_date
+    duration = 5
+    end_date = start_date + duration.days
+    create(:task, indelible: Constants::INDELIBLE_GROUP, indent: 0, batch: batch1,
+           phase: Constants::CONST_CLONE,
+           start_date: start_date,
+           duration: duration,
+           end_date: end_date)
+    create(:task, indelible: Constants::INDELIBLE_CLIP_POT_TAG, batch: batch1,
+           phase: Constants::CONST_CLONE,
+           start_date: start_date,
+           duration: 1)
+    create(:task, indelible: Constants::INDELIBLE_MOVING_TO_TRAY, batch: batch1,
+           phase: Constants::CONST_CLONE,
+           start_date: start_date,
+           duration: 1)
+    create(:task, indelible: Constants::INDELIBLE_STAYING, batch: batch1,
+           phase: Constants::CONST_CLONE,
+           start_date: start_date + 1.days,
+           duration: 4,
+           end_date: end_date)
   end
-  let!(:task2) do
-    create(:task,
-           indelible: Constants::INDELIBLE_MOVING_TO_TRAY,
-           batch: batch1,
-           start_date: batch1.start_date,
-           phase: Constants::CONST_CLONE)
+  let!(:task_staying_veg) do
+    start_date = task_staying_clone.end_date
+    duration = 7
+    end_date = start_date + duration.days
+    create(:task, indelible: Constants::INDELIBLE_GROUP, indent: 0, batch: batch1,
+           phase: Constants::CONST_VEG,
+           start_date: start_date,
+           duration: duration,
+           end_date: end_date)
+    create(:task, indelible: Constants::INDELIBLE_STAYING, batch: batch1,
+           phase: Constants::CONST_VEG,
+           start_date: start_date,
+           duration: duration,
+           end_date: end_date)
   end
-  let!(:task3) do
-    create(:task,
-           indelible: Constants::INDELIBLE_STAYING,
-           batch: batch1,
-           start_date: batch1.start_date + 2.days,
-           phase: Constants::CONST_CLONE)
+  let!(:task_staying_flower) do
+    start_date = task_staying_veg.end_date
+    duration = 10
+    end_date = start_date + duration.days
+    create(:task, indelible: Constants::INDELIBLE_GROUP, indent: 0, batch: batch1,
+           phase: Constants::CONST_FLOWER,
+           start_date: start_date,
+           duration: duration,
+           end_date: end_date)
+    create(:task, indelible: Constants::INDELIBLE_STAYING, batch: batch1,
+           phase: Constants::CONST_FLOWER,
+           start_date: start_date,
+           duration: duration,
+           end_date: end_date)
   end
-  let!(:task4) do
-    create(:task,
-           indelible: Constants::INDELIBLE_GROUP,
-           indent: 0,
-           batch: batch1,
-           start_date: batch1.start_date + 10.days,
-           phase: Constants::CONST_VEG)
-    create(:task,
-           indelible: Constants::INDELIBLE_STAYING,
-           batch: batch1,
-           start_date: batch1.start_date + 10.days,
-           phase: Constants::CONST_VEG)
-  end
-  let!(:task6) do
-    create(:task,
-           indelible: Constants::INDELIBLE_GROUP,
-           indent: 0,
-           batch: batch1,
-           start_date: batch1.start_date + 30.days,
-           phase: Constants::CONST_FLOWER)
-    create(:task,
-           indelible: Constants::INDELIBLE_STAYING,
-           batch: batch1,
-           start_date: batch1.start_date + 30.days,
-           phase: Constants::CONST_FLOWER)
+  let!(:task_staying_harvest) do
+    start_date = task_staying_flower.end_date
+    duration = 3
+    end_date = start_date + duration.days
+    create(:task, indelible: Constants::INDELIBLE_GROUP, indent: 0, batch: batch1,
+           phase: Constants::CONST_HARVEST,
+           start_date: start_date,
+           duration: duration,
+           end_date: end_date)
   end
 
   context ".call" do
     it "Activate batch on start date" do
       Time.use_zone(facility.timezone) do
-        Cultivation::ActivateBatch.call(batch1.start_date)
+        Timecop.freeze(batch1.start_date) do
+          Cultivation::ActivateBatch.call(Time.current)
 
-        result = Cultivation::Batch.find(batch1.id)
-        expect(result.status).to eq 'ACTIVE'
-        expect(result.current_growth_stage).to eq 'clone'
+          result = Cultivation::Batch.find(batch1.id)
+          expect(result.status).to eq 'ACTIVE'
+          expect(result.current_growth_stage).to eq Constants::CONST_CLONE
+        end
       end
     end
 
     it "Update batch growth stage to veg" do
       Time.use_zone(facility.timezone) do
-        Cultivation::ActivateBatch.call(task4.start_date)
+        # pp "veg == #{task_staying_veg.start_date}"
+        # pp "flo == #{task_staying_flower.start_date}"
+        # pp "har == #{task_staying_harvest.start_date}"
+        current_time = Time.zone.local(2018, 2, 7, 12, 0, 0)
+        Timecop.freeze(current_time) do
+          # pp "freeze@#{Time.current}"
+          Cultivation::ActivateBatch.call(Time.current)
 
-        result = Cultivation::Batch.find(batch1.id)
-        expect(result.current_growth_stage).to eq Constants::CONST_VEG
+          result = Cultivation::Batch.find(batch1.id)
+          expect(result.current_growth_stage).to eq Constants::CONST_VEG
+        end
       end
     end
 
     it "Update batch growth stage to flower" do
       Time.use_zone(facility.timezone) do
-        Cultivation::ActivateBatch.call(task6.start_date)
+        # pp "veg == #{task_staying_veg.start_date}"
+        # pp "flo == #{task_staying_flower.start_date}"
+        # pp "har == #{task_staying_harvest.start_date}"
+        current_time = Time.zone.local(2018, 2, 13, 12, 0, 0)
+        Timecop.freeze(current_time) do
+          # pp "freeze@#{Time.current}"
+          Cultivation::ActivateBatch.call(Time.current)
 
-        result = Cultivation::Batch.find(batch1.id)
-        expect(result.current_growth_stage).to eq Constants::CONST_FLOWER
+          result = Cultivation::Batch.find(batch1.id)
+          expect(result.current_growth_stage).to eq Constants::CONST_FLOWER
+        end
+      end
+    end
+
+    it "Update batch growth stage to harvest", focus: true do
+      Time.use_zone(facility.timezone) do
+        # pp "flo == #{task_staying_flower.start_date}"
+        # pp "har == #{task_staying_harvest.start_date}"
+        current_time = Time.zone.local(2018, 2, 23, 12, 0, 0)
+        Timecop.freeze(current_time) do
+          # pp "freeze@#{Time.current}"
+          Cultivation::ActivateBatch.call(Time.current)
+
+          result = Cultivation::Batch.find(batch1.id)
+          expect(result.current_growth_stage).to eq Constants::CONST_HARVEST
+        end
       end
     end
   end

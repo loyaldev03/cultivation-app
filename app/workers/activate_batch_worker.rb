ed2336edf377e6c6a6eb5219b0d2924e97bf23ec
Andy Yong <kwokhou@gmail.com>
2019-04-25 17:42:47 +0800

    Time travel for worker

--- app/workers/activate_batch_worker.rb
+++ app/workers/activate_batch_worker.rb
@@ -2,8 +2,29 @@ class ActivateBatchWorker
   include Sidekiq::Worker
 
   def perform(*args)
-    @logger = Logger.new(STDOUT)
-    @logger.debug "Execute Cultivation::ActivateBatch @ #{Time.current}"
-    Cultivation::ActivateBatch.call(Time.current)
+    if ENV['ENABLE_TIME_TRAVEL'] == 'yes'
+      time_travel
+      @logger = Logger.new(STDOUT)
+      @logger.debug "Execute Cultivation::ActivateBatch @ #{Time.current}"
+      Cultivation::ActivateBatch.call(Time.current)
+      time_travel_return
+    end
+  end
+
+  private
+
+  def time_travel
+    config = System::Configuration.first
+    if config&.enable_time_travel
+      Rails.logger.info 'TIME TRAVEL START'
+      Timecop.travel(config.current_time)
+    else
+      Timecop.return
+    end
+  end
+
+  def time_travel_return
+    Rails.logger.info 'TIME TRAVEL RETURN'
+    Timecop.return
   end
 end

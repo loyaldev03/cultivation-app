7278d3300eab8392b086e25f8276ca6423fe7439
Mr.Worldwide <syahfisal37@yahoo.com>
2019-05-03 19:12:12 +0800

    finished filtration code

--- app/javascript/components/cultivation/tasks_setup/components/TablePlantViewer.js
+++ app/javascript/components/cultivation/tasks_setup/components/TablePlantViewer.js
@@ -1,10 +1,86 @@
 import React from 'react'
 import Tippy from '@tippy.js/react'
+import { set } from 'mobx'
 
 export default class TablePlantViewer extends React.Component {
-  componentDidMount() {}
+  state = {
+    applyAllToggle: false,
+    allLocationChecked: true,
+    filterPlant: [],
+    filterPlantList: [],
+    allClippingValue: 0
+  }
+  componentDidMount() {
+    let filterPlant = this.props.data.data.map(x => {
+      return { name: x.location_code, ticked: true }
+    })
+    let filterPlantList = this.props.data.data.map(x => x.location_code)
+    filterPlantList = [...new Set(filterPlantList)]
+    this.setState({
+      filterPlant: this.getUnique(filterPlant, 'name'),
+      filterPlantList
+    })
+  }
+  getUnique = (arr, comp) => {
+    const unique = arr
+      .map(e => e[comp])
+
+      // store the keys of the unique objects
+      .map((e, i, final) => final.indexOf(e) === i && i)
+
+      // eliminate the dead keys & store unique objects
+      .filter(e => arr[e])
+      .map(e => arr[e])
+
+    return unique
+  }
+  onShowAllLocation = () => {
+    let { allLocationChecked } = this.state
+    if (!allLocationChecked) {
+      let filterPlant = this.props.data.data.map(x => {
+        return { name: x.location_code, ticked: true }
+      })
+      let filterPlantList = this.props.data.data.map(x => x.location_code)
+      filterPlantList = [...new Set(filterPlantList)]
+      this.setState({
+        filterPlant: this.getUnique(filterPlant, 'name'),
+        filterPlantList,
+        allLocationChecked: true
+      })
+    }
+  }
+  onApplyAllClipping = e => {
+    this.props.data.data.map(plant =>
+      this.props.onUpdateOnePlant(plant.plant_id, e)
+    )
+    this.setState({
+      allClippingValue: e.target.value
+    })
+  }
+  onFilterLocation = (e, data) => {
+    const { filterPlant, filterPlantList } = this.state
+    filterPlant[filterPlant.indexOf(data)].ticked = !data.ticked
+    filterPlantList.indexOf(data.name) >= 0
+      ? filterPlantList.splice(filterPlantList.indexOf(data.name), 1)
+      : filterPlantList.push(data.name)
+    console.log(filterPlantList)
+    this.setState({
+      filterPlant,
+      filterPlantList,
+      allLocationChecked: filterPlant.every(plant => plant.ticked === true)
+        ? true
+        : false
+    })
+  }
   render() {
     let { name, data } = this.props.data
+    const {
+      applyAllToggle,
+      allLocationChecked,
+      filterPlant,
+      filterPlantList,
+      allClippingValue
+    } = this.state
     return (
       <div className="mb4">
         <div className="subtitle-2">
@@ -30,10 +106,10 @@ export default class TablePlantViewer extends React.Component {
               <label className="switch">
                 <input
                   type="checkbox"
-                  //checked={applyAllToggle}
-                  // onChange={e =>
-                  //   this.setState({ applyAllToggle: !applyAllToggle })
-                  // }
+                  checked={applyAllToggle}
+                  onChange={e =>
+                    this.setState({ applyAllToggle: !applyAllToggle })
+                  }
                 />
                 <span className="slider round" />
               </label>
@@ -42,65 +118,87 @@ export default class TablePlantViewer extends React.Component {
         </div>
         <div className="mt2">
           <div className="w-100 flex">
-            <div className=" w-25  pt2 bb b--light-gray pb1 mb1">
-              {/* <Tippy
-              trigger="click"
-              content={
-                <div className="bg-white ba b--light-gray br2 ml1 w5 grey overflow-y-auto">
-                  <div className="bg-light-gray pa1">
-                    <input
-                      type="checkbox"
-                      checked={allLocationChecked}
-                      onChange={e => this.onShowAllLocation()}
-                    />{' '}
-                    All
-                  </div>
-                  {trayFilterList.map(x => (
-                    <div className="pa1" key={x.code}>
+            <div className=" w-25  pt2 ">
+              <Tippy
+                trigger="click"
+                content={
+                  <div className="bg-white ba b--light-gray br2 ml1 w5 grey overflow-y-auto">
+                    <div className="bg-light-gray pa1">
                       <input
                         type="checkbox"
-                        checked={x.ticked}
-                        onChange={e => this.onFilterLocation(e, x)}
-                      />
-                      {x.code}
+                        checked={allLocationChecked}
+                        onChange={e => this.onShowAllLocation()}
+                      />{' '}
+                      All
                     </div>
-                  ))}
-                </div>
-              }
-            > */}
-              <i className="material-icons icon--small pointer">filter_list</i>
-              {/* </Tippy> */}
+                    {filterPlant.map(x => (
+                      <div className="pa1" key={x.name}>
+                        <input
+                          type="checkbox"
+                          checked={x.ticked}
+                          onChange={e => this.onFilterLocation(e, x)}
+                        />
+                        {x.name}
+                      </div>
+                    ))}
+                  </div>
+                }
+              >
+                <i className="material-icons icon--small pointer">
+                  filter_list
+                </i>
+              </Tippy>
               Location Code
             </div>
-            <div className=" w-25  pt2 bb b--light-gray pb1 mb1">Plant ID</div>
-            <div
-              className={`fl w-25  pt2 bb b--light-gray  pb1 mb1 ${true &&
-                'flex justify-between'}`}
-            >
+            <div className=" w-25  pt2 ">Plant ID</div>
+            <div className={`fl w-25  pt2 ${true && 'flex justify-between'}`}>
               <span className="">Number of clipping</span>
             </div>
-            <div className="w-25  pt2 bb b--light-gray pb1 mb1">
+            <div className="w-25  pt2 pl3 ">
               Total Clone
               {/* {totalPlantOnForm} */}
             </div>
           </div>
         </div>
-        {data.map(tray => (
-          <div className=" w-100 flex" key={tray.plant_id}>
-            <div className="w-25 pa1">{tray.location_code}</div>
-            <div className="w-25 pa1">{tray.plant_code}</div>
-            <div className="w-20 pa1 tc">
+        <div className="w-100 flex b h2">
+          <div className="w-25 tc bb b--light-gray pb1 mb1">All</div>
+          <div className="w-25 bb b--light-gray pb1 mb1">All Plant</div>
+          <div className="w-25 tc bb b--light-gray pb1 mb1">
+            {applyAllToggle && (
               <input
                 type="number"
-                className="gray w-80 tr b--none"
-                value={tray.quantity}
+                className="input w-100 tr"
+                value={allClippingValue}
                 maxLength="2"
-                // readOnly
-                onChange={e => this.props.onUpdateOnePlant(tray.plant_id, e)}
+                onChange={e => this.onApplyAllClipping(e)}
+                style={{
+                  height: 'auto'
+                }}
               />
-            </div>
+            )}
           </div>
-        ))}
+          <div className="w-25 tc bb b--light-gray pb1 mb1">
+            {data.reduce((a, b) => a + (parseInt(b['quantity']) || 0), 0)}
+          </div>
+        </div>
+        {data.map((tray, i) => {
+          return filterPlantList.indexOf(tray.location_code) >= 0 ? (
+            <div className=" w-100 flex" key={tray.plant_id}>
+              <div className="w-25 pa1">{tray.location_code}</div>
+              <div className="w-25 pa1">{tray.plant_code}</div>
+              <div className="w-20 pa1 tc">
+                <input
+                  type="number"
+                  className="gray w-80 tr b--none"
+                  value={tray.quantity}
+                  maxLength="2"
+                  // readOnly
+                  onChange={e => this.props.onUpdateOnePlant(tray.plant_id, e)}
+                />
+              </div>
+            </div>
+          ) : null
+        })}
       </div>
     )
   }

2d316ae556f147f7172be7dce3fc0f189e57c47e
fathiabdulrahim <fathiabdrahim@yahoo.com>
2019-05-07 13:44:24 +0800

    update styling for the start_time and end_time

--- app/javascript/components/settings/UserDetailsEditor.js
+++ app/javascript/components/settings/UserDetailsEditor.js
@@ -741,7 +741,8 @@ class UserDetailsEditor extends React.PureComponent {
                               Start Time
                             </label>
                             <input
-                              className="db f5 mt2 pa1 black ba b--black-20 br2 outline-0 no-spinner tc"
+                              className="db f6 mt2 black ba b--black-20 br2 outline-0 no-spinner tc"
+                              style={{padding: '.35rem'}}
                               type="time"
                               onChange={e =>
                                 this.onChangeNonExemptAttr(
@@ -758,7 +759,8 @@ class UserDetailsEditor extends React.PureComponent {
                               End Time
                             </label>
                             <input
-                              className="db f5 mt2 pa1 black ba b--black-20 br2 outline-0 no-spinner tc"
+                              className="db f6 mt2 black ba b--black-20 br2 outline-0 no-spinner tc"
+                              style={{ padding: '.35rem' }}
                               type="time"
                               onChange={e =>
                                 this.onChangeNonExemptAttr(

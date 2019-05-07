00435c4e3f36b26771f54863e19f0fcabf046b00
fathiabdulrahim <fathiabdrahim@yahoo.com>
2019-05-07 13:44:51 +0800

    run formatter

--- app/javascript/components/settings/UserDetailsEditor.js
+++ app/javascript/components/settings/UserDetailsEditor.js
@@ -742,7 +742,7 @@ class UserDetailsEditor extends React.PureComponent {
                             </label>
                             <input
                               className="db f6 mt2 black ba b--black-20 br2 outline-0 no-spinner tc"
-                              style={{padding: '.35rem'}}
+                              style={{ padding: '.35rem' }}
                               type="time"
                               onChange={e =>
                                 this.onChangeNonExemptAttr(

3a3f347507dde0e419857745a8c1743f5041350b
fathiabdulrahim <fathiabdrahim@yahoo.com>
2019-05-09 15:49:11 +0800

    run formatter

--- app/javascript/components/cultivation/dashboards/IssuesDashboardApp.js
+++ app/javascript/components/cultivation/dashboards/IssuesDashboardApp.js
@@ -56,14 +56,17 @@ class IssuesDashboard extends React.Component {
         accessor: 'severity',
         className: 'justify-center ttu',
         minWidth: 88,
-        Cell: props => 
-          <span 
+        Cell: props => (
+          <span
             className={classNames(`fw6 red`, {
-              'purple': props.value === 'severe',
-              'yellow': props.value === 'medium',
-              'red': props.value === 'high',
+              purple: props.value === 'severe',
+              yellow: props.value === 'medium',
+              red: props.value === 'high'
             })}
-          >{props.value}</span>
+          >
+            {props.value}
+          </span>
+        )
       },
       {
         headerClassName: 'tl',

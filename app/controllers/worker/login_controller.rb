25cfb488440a5273ab9620a10e6afb745e4febfa
fathiabdulrahim <fathiabdrahim@yahoo.com>
2019-05-15 14:54:23 +0800

    added data for role and user

--- app/controllers/worker/login_controller.rb
+++ app/controllers/worker/login_controller.rb
@@ -3,6 +3,17 @@ class Worker::LoginController < ApplicationController
   before_action :check_ip_whitelist
 
   def index
+    if @ip_included
+      @roles = Common::Role.pluck(:name)
+      if params[:role].present?
+        if params[:role] == 'all'
+          @users = User.all
+        else
+          role = Common::Role.find_by(name: params[:role])
+          @users = User.all_in(roles: [role.id]) if role.present?
+        end
+      end
+    end
   end
 
   private

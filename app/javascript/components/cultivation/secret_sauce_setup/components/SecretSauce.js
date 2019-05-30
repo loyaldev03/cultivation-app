fac5f962e4d6971217e0982aa55f9ec9294888bd
fathiabdulrahim <fathiabdrahim@yahoo.com>
2019-05-30 11:25:38 +0800

    update new layout for secret sauce

--- app/javascript/components/cultivation/secret_sauce_setup/components/SecretSauce.js
+++ app/javascript/components/cultivation/secret_sauce_setup/components/SecretSauce.js
@@ -45,62 +45,92 @@ class SecretSauce extends React.Component {
     const { phase, tabIndex } = this.state
     return (
       <React.Fragment>
-        <div className="flex">
-          <div className="w4 pt3">
-            <ul className="list mt4 pl0">
-              {['clone', 'veg1', 'veg2', 'flower'].map(x => {
-                return (
-                  <li key={x} className="ml2 mr4 mv2">
-                    <a
-                      className={classNames(
-                        'f6 f5-ns db pa2 link dim mid-gray pointer ttc',
-                        {
-                          'ba b--orange orange': phase === x,
-                          'ba b--light-grey': phase !== x
-                        }
-                      )}
-                      onClick={this.onSwitchPhase(x)}
-                    >
-                      {x}
-                    </a>
-                  </li>
-                )
-              })}
-            </ul>
+        <div class="flex justify-center">
+          <span className="f6 b flex items-center mr2">Growth Schedule : </span>
+          <div className="bg-orange f6 white pa2 br1 pointer">
+            Clone (5w)
           </div>
-          <div className="w-80">
-            <Tabs
-              className="react-tabs--primary react-tabs--secret-sause"
-              selectedIndex={tabIndex}
-              onSelect={this.onSelectTab}
-            >
-              <TabList>
-                {SAUSE_TABS.map(x => (
-                  <Tab key={x[0]}>{x[1]}</Tab>
-                ))}
-              </TabList>
-              {SAUSE_TABS.map(x => (
-                <TabPanel key={x[0]}>
-                  {x[0] === 'nutrients' && (
-                    <div className="pa3">
-                      <span className="pa1 f6 f5-ns db mb3 ttc fw6">
-                        {phase}
-                      </span>
-                      <div className="flex">
-                        <NutrientList className="flex flex-column" />
-                        <NutrientsAdded
-                          className="flex flex-column"
-                          phase={phase}
-                        />
-                      </div>
-                    </div>
-                  )}
-                  {x[0] !== 'nutrients' && <div className="h5">&nbsp;</div>}
-                </TabPanel>
-              ))}
-            </Tabs>
+          <div className="bg-white f6 grey pa2 bt bb b--light-gray pointer">
+            Veg1 (3w)
+          </div>
+          <div className="bg-white f6 grey pa2 bt bb b--light-gray pointer">
+            Veg2 (n/a)
+          </div>
+          <div className="bg-white f6 grey pa2 bt bb br b--light-gray pointer">
+            Flower (8w)
           </div>
         </div>
+        <div className="mt4 flex">
+          <div className="w-20 pa3 mr2 h7">
+            <div className="f6 h2 mt3"></div>
+            <div className="f6 h2 mt3">Light hours (per day)</div>
+            <div className="f6 h2 mt3">Temperature (day/night)</div>
+            <div className="f6 h2 mt3">Humidity level</div>
+            <div className="f6 h2 mt3">Water intake</div>
+            <div className="f6 h2 mt3">Nutrients</div>
+            <div className="f6 h2 mt3">Active ingredients</div>
+          </div>
+          <div className="w-25 pa3 mr2 h7 tc">
+            <div className="f6 h2 mt3">Week 1</div>
+            <div className="f6 h2 mt3">12h</div>
+            <div className="f6 h2 mt3">77/64 F</div>
+            <div className="f6 h2 mt3">75%</div>
+            <div className="f6 h2 mt3">1l per plant 1,6 ph</div>
+            <div className="f6 h2 mt3">Compose tea x 125ml, FP Grow x 4l</div>
+            <div className="f6 h2 mt3">Na 8,2% CI 3,4% Ph 2,1%</div>
+          </div>
+          <div className="w-25 pa3 mr2 h7 tc">
+            <div className="f6 h2 mt3">Week 2</div>
+            <div className="f6 h2 mt3">12h</div>
+            <div className="f6 h2 mt3">77/64 F</div>
+            <div className="f6 h2 mt3">75%</div>
+            <div className="f6 h2 mt3">1l per plant 1,6 ph</div>
+            <div className="f6 h2 mt3">--</div>
+            <div className="f6 h2 mt3">Na 8,2% CI 3,4% Ph 2,1%</div>
+          </div>
+          <div className="w-25 pa3 mr2 h7 tc">
+            <div className="f6 h2 mt3">Week 3</div>
+            <div className="f6 h2 mt3">Light hours (per day)</div>
+            <div className="f6 h2 mt3">77/64 F</div>
+            <div className="f6 h2 mt3">75%</div>
+            <div className="f6 h2 mt3">1l per plant 1,6 ph</div>
+            <div className="f6 h2 mt3">FP Grow x 125ml</div>
+            <div className="f6 h2 mt3">--</div>
+          </div>
+          <div className="w-25 pa3 mr2 h7 tc">
+            <div className="f6 h2 mt3">Week 4</div>
+            <div className="f6 h2 mt3">Light hours (per day)</div>
+            <div className="f6 h2 mt3">77/64 F</div>
+            <div className="f6 h2 mt3">40%</div>
+            <div className="f6 h2 mt3">1l per plant 1,6 ph</div>
+            <div className="f6 h2 mt3">FP Grow x 125ml</div>
+            <div className="f6 h2 mt3">Na 8,2% CI 3,4% Ph 2,1%</div>
+          </div>
+          <div className="w-25 pa3 mr2 h7 tc">
+            <div className="f6 h2 mt3">Week 5</div>
+            <div className="f6 h2 mt3">Light hours (per day)</div>
+            <div className="f6 h2 mt3">77/64 F</div>
+            <div className="f6 h2 mt3">35%</div>
+            <div className="f6 h2 mt3">1l per plant 1,6 ph</div>
+            <div className="f6 h2 mt3">FP Grow x 125ml</div>
+            <div className="f6 h2 mt3">--</div>
+          </div>
+          <div className="w-25 pa3 mr2 h7 tc">
+            <div className="f6 h2 mt3">Week 6</div>
+            <div className="flex h5 items-center">
+              <div className="center">
+                <i className="material-icons bg-orange white br-100 pa2 pointer">edit</i>
+              </div>
+            </div>
+            {/* <div className="f6 h2 mt3">Light hours (per day)</div>
+            <div className="f6 h2 mt3">77/64 F</div>
+            <div className="f6 h2 mt3">35%</div>
+            <div className="f6 h2 mt3">1l per plant 1,6 ph</div>
+            <div className="f6 h2 mt3">FP Grow x 125ml</div>
+            <div className="f6 h2 mt3">--</div> */}
+          </div>
+        </div>
+
       </React.Fragment>
     )
   }

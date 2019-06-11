7d3b48f335de43f03db290afc217bc2300f027f6
fatinf <fatinfarhana.badrulhisham@gmail.com>
2019-06-11 23:44:28 +0800

    run formatter

--- app/javascript/components/utils/InputBarcode.js
+++ app/javascript/components/utils/InputBarcode.js
@@ -2,7 +2,7 @@ import React, { useState, forwardRef } from 'react'
 import PropTypes from 'prop-types'
 import { ImgBarcode } from './Icons'
 import { launchBarcodeScanner } from '../utils/BarcodeScanner'
-import BarCodeComponent from './BarcodeComponent';
+import BarCodeComponent from './BarcodeComponent'
 
 const InputBarcode = forwardRef(
   (
@@ -22,7 +22,7 @@ const InputBarcode = forwardRef(
   ) => {
     let scanner = null
     let scannerRef = null
-    const [hidden, setHidden] = useState(false);
+    const [hidden, setHidden] = useState(false)
     const onShowScanner = e => {
       setHidden(!hidden)
     }
@@ -62,7 +62,7 @@ const InputBarcode = forwardRef(
             </React.Fragment>
           )}
         </div>
-        {hidden && <BarCodeComponent/>}
+        {hidden && <BarCodeComponent />}
         {error && <span className="f7 i red dib pv1">{error}</span>}
         {multiple ? (
           <div className="w-100 mb2 mt2 flex justify-end">

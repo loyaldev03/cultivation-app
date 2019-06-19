dd418b91f94839815ad0e3faa359b15bd1b90947
Karg Weng <karg@betacode.io>
2019-06-19 18:57:37 +0800

    Added code back to grow medium

--- app/models/common/grow_method.rb
+++ app/models/common/grow_method.rb
@@ -3,9 +3,16 @@ module Common
     include Mongoid::Document
     include Mongoid::Timestamps::Short
 
+    field :code, type: String
     field :name, type: String
     field :is_active, type: Boolean
 
     scope :active, -> { where(is_active: true) }
+
+    before_save :generate_code
+
+    def generate_code
+      self.code = name.underscore.gsub(' ', '_')
+    end
   end
 end

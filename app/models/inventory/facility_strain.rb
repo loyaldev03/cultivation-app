c3b96dec25f4da5d1f29bb74388034398b79237d
Andy Yong <kwokhou@gmail.com>
2019-06-20 17:28:43 +0800

    Added worker to update strains to Metrc

--- app/models/inventory/facility_strain.rb
+++ app/models/inventory/facility_strain.rb
@@ -9,14 +9,15 @@ module Inventory
     include Mongoid::Document
     include Mongoid::Timestamps::Short
 
-    field :thc, type: Float, default: -> { 0 }  # FIXME: incorrect data type
-    field :cbd, type: Float, default: -> { 0 }  # FIXME: incorrect data type
+    field :thc, type: Float, default: -> { 0 }
+    field :cbd, type: Float, default: -> { 0 }
     field :strain_name, type: String
     field :strain_type, type: String
 
     field :indica_makeup, type: Integer
     field :sativa_makeup, type: Integer
     field :testing_status, type: String
+    field :metrc_id, type: Integer
 
     belongs_to :facility, class_name: 'Facility'
     belongs_to :created_by, class_name: 'User'

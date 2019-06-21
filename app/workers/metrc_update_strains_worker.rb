f9500da1a854973e25b1960f5e75b7c2a3315596
Andy Yong <kwokhou@gmail.com>
2019-06-21 10:42:36 +0800

    Fixes strains metrc api expecting json array

--- app/workers/metrc_update_strains_worker.rb
+++ app/workers/metrc_update_strains_worker.rb
@@ -24,13 +24,16 @@ class MetrcUpdateStrainsWorker
                             local_strains)
 
     # Detect changes and update in Metrc
-    # update_strains_on_metrc(facility.site_license,
-    #                         local_strains,
-    #                         remote_strains)
+    update_strains_on_metrc(facility.site_license,
+                            local_strains,
+                            remote_strains)
 
     # Update Metrc Id to local record
-    # update_local_metrc_ids(facility.site_license,
-    #                        local_strains)
+    update_local_metrc_ids(facility.site_license,
+                           local_strains)
+    true
+    # rescue RestClient::ExceptionWithResponse => e
+    #   pp JSON.parse(e.response.body)
   end
 
   def create_strains_on_metrc(site_license, new_strains_name, local_strains)
@@ -39,15 +42,17 @@ class MetrcUpdateStrainsWorker
         found = local_strains.detect { |i| i.strain_name == s }
         if found&.metrc_id.nil?
           # Only create new strain in when no metrc_id
+          # Rule: sativa + indica should be 100
+          sativa_pct = 100 - (found.indica_makeup&.to_f || 0)
           params = {
             "Name": found.strain_name,
             "TestingStatus": 'None',
             "ThcLevel": found.thc,
             "CbdLevel": found.cbd,
             "IndicaPercentage": found.indica_makeup&.to_f,
-            "SativaPercentage": found.sativa_makeup&.to_f,
+            "SativaPercentage": sativa_pct,
           }
-          MetrcApi.create_strains(site_license, params)
+          MetrcApi.create_strains(site_license, [params])
         end
       end
     end
@@ -57,7 +62,9 @@ class MetrcUpdateStrainsWorker
     if local_strains.any?
       local_strains.each do |s|
         found = remote_strains.detect { |i| i['Name'] == s.strain_name }
-        if found && s.metrc_id
+        # Rule: sativa + indica should be 100
+        sativa_pct = 100 - (s.indica_makeup&.to_f || 0)
+        if found && s.metrc_id && !equal?(s, found)
           params = {
             "Id": s.metrc_id,
             "Name": s.strain_name,
@@ -65,12 +72,18 @@ class MetrcUpdateStrainsWorker
             "ThcLevel": s.thc,
             "CbdLevel": s.cbd,
             "IndicaPercentage": s.indica_makeup&.to_f,
-            "SativaPercentage": s.sativa_makeup&.to_f,
+            "SativaPercentage": sativa_pct,
           }
-          MetrcApi.update_strains(site_license, params)
+          MetrcApi.update_strains(site_license, [params])
         end
       end
     end
+  end
+
+  def equal?(model_strain, hash_strain)
+    model_strain.thc == hash_strain['ThcLevel'] &&
+      model_strain.cbd == hash_strain['CbdLevel'] &&
+      model_strain.sativa_makeup.to_f.equal?(hash_strain['SativaPercentage'])
   end
 
   def update_local_metrc_ids(site_license, local_strains)

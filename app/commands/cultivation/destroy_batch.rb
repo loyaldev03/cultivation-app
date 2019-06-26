e02f0e0b08cb9bd0a8a56648d1ff2bc9fc9f53e1
Andy Yong <kwokhou@gmail.com>
2019-06-26 12:21:28 +0800

    Destroy batch would destroy more related stuffs to batch

--- app/commands/cultivation/destroy_batch.rb
+++ app/commands/cultivation/destroy_batch.rb
@@ -15,9 +15,15 @@ module Cultivation
       @batch = Cultivation::Batch.find(@batch_id)
       if @batch.status == Constants::BATCH_STATUS_DRAFT || Constants::BATCH_STATUS_SCHEDULED
         @batch.delete
-        Cultivation::Task.delete_all({batch_id: @batch_id})
-        Cultivation::TrayPlan.delete_all({batch_id: @batch_id})
-        Issues::Issue.delete_all({cultivation_batch_id: @batch_id})
+        Cultivation::Task.delete_all(batch_id: @batch_id)
+        Cultivation::TrayPlan.delete_all(batch_id: @batch_id)
+        Cultivation::ProductTypePlan.delete_all(batch_id: @batch_id)
+        Issues::Issue.delete_all(cultivation_batch_id: @batch_id)
+        Inventory::HarvestBatch.delete_all(cultivation_batch_id: @batch_id)
+        Inventory::ItemTransaction.delete_all(cultivation_batch_id: @batch_id)
+        Inventory::Plant.delete_all(cultivation_batch_id: @batch_id)
+        Notification.delete_all(notifiable_id: @batch_id)
+        Notification.delete_all(alt_notifiable_id: @batch_id)
         @batch_id
       else
         errors.add(:batch, 'Unable to delete an active batch')

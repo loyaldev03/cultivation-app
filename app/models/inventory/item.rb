636712b37293ea45dee42b54013de2b3cfa5d62e
Andy Yong <kwokhou@gmail.com>
2019-07-02 17:28:32 +0800

    Updated Item model to include facility

--- app/models/inventory/item.rb
+++ app/models/inventory/item.rb
@@ -5,7 +5,7 @@ module Inventory
 
     # This is exact copy of data from metrc
 
-    field :metrc_id, type: Integer # Id used by Metrc when updating
+    field :metrc_id, type: Integer # Item Id used by Metrc when updating
     field :name, type: String
     field :product_category_name, type: String # => item_category
     field :product_category_type, type: String # => item_category
@@ -13,7 +13,7 @@ module Inventory
     field :default_lab_testing_state, type: String
     field :uom_name, type: String
     field :approval_status, type: String
-    field :strain_id, type: Integer
+    field :metrc_strain_id, type: Integer # Strain Id used by Metrc
     field :strain_name, type: String
     field :administration_method, type: String
     field :unit_cbd_percent, type: Float
@@ -32,5 +32,7 @@ module Inventory
     field :unit_quantity_uom_name, type: String
     field :ingredients, type: String
     field :is_used, type: Boolean, default: -> { false }
+
+    belongs_to :facility, class_name: 'Facility'
   end
 end

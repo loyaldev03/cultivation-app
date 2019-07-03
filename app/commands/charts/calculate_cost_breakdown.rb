2c6c8a2f8a412d36da54101f0734563ade97f25b
fathiabdulrahim <fathiabdrahim@yahoo.com>
2019-07-03 11:10:43 +0800

    run formatter

---
+++ app/commands/charts/calculate_cost_breakdown.rb
@@ -0,0 +1,25 @@
+module Charts
+  class CalculateCostBreakdown
+    prepend SimpleCommand
+
+    def initialize(current_user, args = {})
+      @user = current_user
+      @args = args
+    end
+
+    def call
+      month = Date.parse("#{args[:month]} #{args[:year]}")
+      tasks = Cultivation::Task.where(:start_date.lte => month.beginning_of_month, :end_date.gte => month.end_of_month)
+      material_cost = 0
+      labour_cost = 0
+      tasks.each do |task|
+        labour_cost += task.actual_labor_cost
+        material_cost += task.actual_material_cost
+      end
+      {
+        material_cost: material_cost,
+        labour_cost: labour_cost,
+      }
+    end
+  end
+end

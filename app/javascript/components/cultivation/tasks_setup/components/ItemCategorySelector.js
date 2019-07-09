1aa0578bbf4d2b5ee3359a2208cb69350eb0cbe7
Andy Yong <kwokhou@gmail.com>
2019-07-09 15:17:00 +0800

    Update Package Plan creation to use Item Category

--- app/javascript/components/cultivation/tasks_setup/components/ItemCategorySelector.js
+++ app/javascript/components/cultivation/tasks_setup/components/ItemCategorySelector.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { observer } from 'mobx-react'
 import CategoryStore from '../../../settings/ItemCategoryStore'
 import { selectStyles } from '../../../utils'
+import reactSelectStyle from '../../../utils/reactSelectStyle'
 import Select from 'react-select'
 
 @observer
@@ -10,11 +11,24 @@ class ItemCategorySelector extends React.Component {
     selectedCategory: ''
   }
 
-  componentDidMount() {
-    CategoryStore.loadCategories()
+  async componentDidMount() {
+    await CategoryStore.loadCategories()
+    if (CategoryStore.isDataLoaded) {
+      CategoryStore.excludes = this.props.excludes
+    }
+  }
+
+  async componentDidUpdate(prevProps) {
+    const { value } = this.props
+    if (value && value !== prevProps.productType) {
+      const res = CategoryStore.getCategoryByName(value)
+    }
   }
 
   onChange = selectedCategory => {
+    if (this.props.onChange) {
+      this.props.onChange(selectedCategory)
+    }
     this.setState({ selectedCategory })
   }
 
@@ -23,6 +37,7 @@ class ItemCategorySelector extends React.Component {
   }
 
   render() {
+    const { excludes } = this.props
     return (
       <div>
         <Select

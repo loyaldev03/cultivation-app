2da26ac4da113bb2c2f56859963629e7080db2de
Andy Yong <kwokhou@gmail.com>
2019-07-09 17:25:35 +0800

    Update worker to generate item

--- app/workers/generate_item_from_packageplan.rb
+++ app/workers/generate_item_from_packageplan.rb
@@ -5,6 +5,25 @@ class GenerateItemFromPackageplan
   def perform(batch_id)
     @batch_id = batch_id
     # get list of ProductType Plan by batch_id
+    product_plan_items = Inventory::QueryProductPackagePlans.call(
+      batch_id: @batch_id.to_bson_id,
+    )
+
+    if product_plan_items.any
+      product_plan_items.each do |p|
+        item_name = "#{p.product_type} - #{p.strain} - #{p.no} - #{p.harvest_name}"
+        item = Inventory::Item.find_or_initialize_by(
+          name: item_name,
+          product_category_name: p.product_type,
+          uom_name: p.product_uom,
+          strain_name: p.strain,
+        )
+        logger.debug "\033[31m >>> >>> \033[0m"
+        logger.debug item
+        logger.debug "\033[31m <<< <<< \033[0m"
+      end
+    end
+
     # get list of Items for batch_id
     # generate item from each plan, no duplicate
     logger.debug 'Perform GenerateItemFromPackageplan'

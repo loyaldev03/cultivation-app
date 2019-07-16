2f9aeb3a81c5f0729a6f86ea7df8aed5a3e9a544
Andy Yong <kwokhou@gmail.com>
2019-07-16 15:48:06 +0800

    Refactor to async await

--- app/javascript/components/utils/BarcodeComponent.js
+++ app/javascript/components/utils/BarcodeComponent.js
@@ -7,46 +7,50 @@ const codeReader = new BrowserBarcodeReader()
 class BarCodeComponent extends React.Component {
   constructor(props) {
     super(props)
-    this.selectedVideoDevice = ''
+    this.selectedInput = ''
+    this.webcamInput = ''
   }
 
-  componentDidMount() {
-    codeReader.getVideoInputDevices().then(videoInputDevices => {
-      this.selectedVideoDevice = videoInputDevices[0].deviceId
-      const sourceSelect = document.getElementsByClassName('barcode_cam')
-      let selectedDeviceId = videoInputDevices[0].deviceId
-      if (videoInputDevices.length > 1) {
-        videoInputDevices.forEach(element => {
-          const sourceOption = document.createElement('option')
-          sourceOption.text = element.label
-          sourceOption.value = element.deviceId
-          sourceSelect.appendChild(sourceOption)
-        })
-        sourceSelect.onchange = () => {
-          selectedDeviceId = sourceSelect.value
-        }
-        const sourceSelectPanel = document.getElementById('sourceSelectPanel')
-        sourceSelectPanel.style.display = 'block'
+  async componentDidMount() {
+    const videoDevices = await codeReader.getVideoInputDevices()
+    this.selectedInput = videoDevices[0].deviceId
+    const sourceSelect = document.getElementsByClassName('barcode_cam')
+    let selectedDeviceId = videoDevices[0].deviceId
+    if (videoDevices.length > 1) {
+      videoDevices.forEach(element => {
+        const sourceOption = document.createElement('option')
+        sourceOption.text = element.label
+        sourceOption.value = element.deviceId
+        sourceSelect.appendChild(sourceOption)
+      })
+      sourceSelect.onchange = () => {
+        selectedDeviceId = sourceSelect.value
       }
-      codeReader
-        .decodeFromInputVideoDevice(selectedDeviceId)
-        .then(result => {
-          if (result) {
-            this.props.onBarcodeScan(result.text)
-            this.props.onShowScanner()
-          }
-        })
-        .catch(err => {
-          console.error(err)
-        })
-      console.log(
-        `Started continous decode from camera with id ${selectedDeviceId}`
-      )
-    })
+      const sourceSelectPanel = document.getElementById('sourceSelectPanel')
+      sourceSelectPanel.style.display = 'block'
+    }
+    codeReader
+      .decodeFromInputVideoDevice(selectedDeviceId)
+      .then(result => {
+        if (result) {
+          this.props.onBarcodeScan(result.text)
+          this.props.onShowScanner()
+        }
+      })
+      .catch(err => {
+        console.error(err)
+      })
   }
 
   render() {
-    return <Webcam className="barcode_cam" height={350} width={350} />
+    return (
+      <Webcam
+        ref={input => (this.webcamInput = input)}
+        className="barcode_cam"
+        height={350}
+        width={350}
+      />
+    )
   }
 }
 

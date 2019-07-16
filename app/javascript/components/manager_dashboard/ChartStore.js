c7ead6cb29fe6be3aadfc2c52bf379aacf0bbe7f
doodrom@gmail.com <fatinfarhana.badrulhisham@gmail.com>
2019-07-16 22:34:02 +0800

    update prettier-eslint

--- app/javascript/components/manager_dashboard/ChartStore.js
+++ app/javascript/components/manager_dashboard/ChartStore.js
@@ -68,7 +68,6 @@ class ChartStore {
     }
   }
 
-
   @action
   async issueList() {
     this.isLoading = true
@@ -94,17 +93,19 @@ class ChartStore {
         const issue_no = t.issue_no.toString().toLowerCase()
         const batch = t.batch.toString().toLowerCase()
         const title = t.title.toString().toLowerCase()
-        const result = issue_no.includes(this.filter.toLowerCase()) || batch.includes(this.filter.toLowerCase()) || title.includes(this.filter.toLowerCase())
+        const result =
+          issue_no.includes(this.filter.toLowerCase()) ||
+          batch.includes(this.filter.toLowerCase()) ||
+          title.includes(this.filter.toLowerCase())
         return result
       })
-
     } else {
       return this.data_issue_list
     }
   }
 
   @action
-  async loadBatchDistribution(date,label) {
+  async loadBatchDistribution(date, label) {
     this.isLoading = true
     this.batch_distribution_loaded = false
     const url = `/api/v1/dashboard_charts/batch_distribution?date=${date}&label=${label}`
@@ -128,13 +129,13 @@ class ChartStore {
         labels: this.data_batch_distribution.map(d => d.phase),
         datasets: [
           {
-            label: "Batch",
+            label: 'Batch',
             data: this.data_batch_distribution.map(d => d.batch_count),
-            backgroundColor: "rgba(241, 90, 34, 1)"
+            backgroundColor: 'rgba(241, 90, 34, 1)'
           },
           {
-            label: "Plant",
-            data:  this.data_batch_distribution.map(d => d.plant_count),
+            label: 'Plant',
+            data: this.data_batch_distribution.map(d => d.plant_count),
             type: 'line',
             pointRadius: 0,
             hoverRadius: 0
--- app/javascript/components/manager_dashboard/IssueList.js
+++ app/javascript/components/manager_dashboard/IssueList.js
@@ -13,33 +13,38 @@ export default class UnassignedTask extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <div className="flex justify-between mb2" >
+        <div className="flex justify-between mb2">
           <h1 className="f5 fw6">Issues</h1>
-          <input placeholder="Filter Issue" className="input w6"  
+          <input
+            placeholder="Filter Issue"
+            className="input w6"
             onChange={e => {
               ChartStore.filter = e.target.value
-            }}>
-          </input>
+            }}
+          />
         </div>
-       
-        
+
         <div className="overflow-y-scroll" style={{ height: 280 + 'px' }}>
           {ChartStore.filterIssueList.map(e => (
             <React.Fragment>
               <div className="mb4">
-                <a className="no-underline" href={`/cultivation/batches/${e.batch_id}/issues`}>
-                  <span className="f5 grey">Issue {formatIssueNo(e.issue_no)}, Batch {e.batch} </span>
+                <a
+                  className="no-underline"
+                  href={`/cultivation/batches/${e.batch_id}/issues`}
+                >
+                  <span className="f5 grey">
+                    Issue {formatIssueNo(e.issue_no)}, Batch {e.batch}{' '}
+                  </span>
                   <span className="f7 green ttu">{e.status}</span>
-                  <br></br>
+                  <br />
                   <span className="f7 mt1 grey">{e.created_at}</span>
-                  <br></br> 
+                  <br />
                   <span className="black">{e.title}</span>
                 </a>
               </div>
             </React.Fragment>
           ))}
         </div>
-
       </React.Fragment>
     )
   }

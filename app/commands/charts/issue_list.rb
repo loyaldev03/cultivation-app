3affa146d5bffd07d8d1c6641ecf346a209e0736
fathiabdulrahim <fathiabdrahim@yahoo.com>
2019-07-17 12:12:48 +0800

    fix dashboard

--- app/commands/charts/issue_list.rb
+++ app/commands/charts/issue_list.rb
@@ -12,8 +12,8 @@ module Charts
         {
           id: issue.id.to_s,
           issue_no: issue.issue_no,
-          batch: issue.cultivation_batch.batch_no,
-          batch_id: issue.cultivation_batch_id.to_s,
+          batch: issue&.cultivation_batch&.batch_no,
+          batch_id: issue&.cultivation_batch_id&.to_s,
           created_at: issue.c_at.strftime('%-d %B, %-l.%M %P'),
           status: issue.status,
           title: issue.title,

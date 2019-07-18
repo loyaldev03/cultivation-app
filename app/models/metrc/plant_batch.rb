73c94846fce4812198657973b9c3231ff22c6128
Andy Yong <kwokhou@gmail.com>
2019-07-18 11:34:12 +0800

    Updated model for metrc plant batch

--- app/models/metrc/plant_batch.rb
+++ app/models/metrc/plant_batch.rb
@@ -3,15 +3,20 @@ module Metrc
     include Mongoid::Document
     include Mongoid::Timestamps::Short
 
-    field :metrc_tag, type: String # this is plant tag
-    field :metrc_id, type: Integer # this is from metrc
-    field :plant_type, type: String # "Clone" / "Seed"
+    # Fields update after batch scheduled
+    field :batch_id, type: BSON::ObjectId
+    field :lot_no, type: Integer
     field :count, type: String # Number of plants in this lot, max 100
     field :strain, type: String
-    field :room, type: String # Room name
+    field :plant_type, type: String # "Clone" / "Seed"
     field :actual_date, type: String # Date when this batch become active "2018-12-15"
+    field :metrc_tag, type: String # this is plant tag
 
-    field :lot_no, type: Integer
-    field :batch_id, type: BSON::ObjectId
+    # Update when user has verified metrc tag has been consumed from inventory
+    field :metrc_tag_verified, type: Boolean, default: -> { false }
+
+    # Fields update after push to metrc
+    field :room, type: String # Room name
+    field :metrc_id, type: Integer # this is from metrc
   end
 end

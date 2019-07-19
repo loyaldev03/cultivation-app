c1865914aab7cb26da7dfab58c95d9ed9fd7198a
Andy Yong <kwokhou@gmail.com>
2019-07-19 09:07:32 +0800

    Added command to update available tag to assigned

---
+++ app/commands/inventory/update_metrc_tags_assigned.rb
@@ -0,0 +1,29 @@
+module Inventory
+  class UpdateMetrcTagsAssigned
+    prepend SimpleCommand
+
+    def initialize(args = {})
+      args = {
+        facility_id: nil,
+        metrc_tags: [],
+      }.merge(args)
+
+      raise ArgumentError, 'facility_id is empty' if args[:facility_id].nil?
+      raise ArgumentError, 'metrc_tags is empty' if args[:metrc_tags].blank?
+      raise ArgumentError, 'metrc_tags is not Array' unless args[:metrc_tags].is_a?(Array)
+
+      @facility_id = args[:facility_id]&.to_bson_id
+      @metrc_tags = args[:metrc_tags]
+    end
+
+    def call
+      Inventory::MetrcTag.where(
+        facility_id: @facility_id,
+        status: Constants::METRC_TAG_STATUS_AVAILABLE,
+        :tag.in => @metrc_tags,
+      ).update_all(
+        status: Constants::METRC_TAG_STATUS_ASSIGNED,
+      )
+    end
+  end
+end

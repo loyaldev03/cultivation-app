a0b36a2f7c6bac87a76aed8dc7945e6b51871529
anissoraya <anissoraya1028@gmail.com>
2019-07-25 18:04:50 +0800

    added chart for inventory plant screen

---
+++ app/javascript/components/inventory/plant_setup/plant_charts/PlantStore.js
@@ -0,0 +1,66 @@
+import { observable, action, computed} from 'mobx'
+import {
+  httpGetOptions
+} from '../../../utils'
+
+function parseTask(taskAttributes) {
+  return Object.assign(taskAttributes)
+}
+
+class PlantStore {
+  @observable data_plant_distribution = []
+  @observable plant_distribution_loaded = false
+
+  @action
+  async loadPlantDistribution(range, facility_id) {
+    this.isLoading = true
+    this.plant_distribution_loaded = false
+    const url = `/api/v1/dashboard_charts/batch_distribution?range=${range}&facility_id=${facility_id}`
+    try {
+      const response = await (await fetch(url, httpGetOptions)).json()
+      if (response) {
+        this.data_plant_distribution = response
+        this.plant_distribution_loaded = true
+      } else {
+        this.data_plant_distribution = []
+      }
+    } catch (error) {
+      console.error(error)
+    } finally {
+    }
+  }
+
+  @computed get plantDistribution() {
+    if (this.plant_distribution_loaded) {
+      let final_result = {
+        labels: this.data_plant_distribution.query_batches.map(d => d.phase),
+        datasets: [
+          {
+            label: 'Plant',
+            data: this.data_plant_distribution.query_batches.map(
+              d => d.plant_count
+            ),
+            backgroundColor: 'rgba(241, 90, 34, 1)'
+          },
+          {
+            label: 'Batch',
+            data: this.data_plant_distribution.query_batches.map(
+              d => d.batch_count
+            ),
+            type: 'line',
+            pointRadius: 0,
+            hoverRadius: 0
+          }
+        ]
+      }
+
+      return final_result
+    } else {
+      return {}
+    }
+  }
+}
+
+const plantStore = new PlantStore()
+
+export default plantStore
---
+++ app/javascript/components/inventory/plant_setup/plant_charts/PlantWidgetApp.js
@@ -0,0 +1,36 @@
+import React, { memo, useState, lazy, Suspense } from 'react'
+import { observer } from 'mobx-react'
+import PlantByPhaseWidget from './PlantByPhaseWidget'
+import PlantByRoomWidget from './PlantByRoomWidget'
+@observer
+class PlantWidgetApp extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div className="flex h-50 mw1200 w-100 mb4">
+        <div className="w-50">
+          <div
+            className="ba b--light-gray pa3 bg-white br2 mr3"
+            style={{ height: 400 + 'px' }}
+          >
+            <PlantByPhaseWidget facility_id={this.props.facility_id}/>
+          </div>
+        </div>
+        <div className="w-50">
+          <div
+            className="ba b--light-gray pa3 bg-white br2 mr3"
+            style={{ height: 400 + 'px' }}
+          >
+            <h1 className="f5 fw6 dark-grey">Plant Distribution by Room</h1>
+            <PlantByRoomWidget facility_id={this.props.facility_id} />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default PlantWidgetApp

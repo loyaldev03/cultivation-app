2c1571d755a709718b9627a371aa2b0a1ba44418
Anis Soraya <anissoraya1028@gmail.com>
2019-08-05 12:48:07 +0800

    attrition

--- app/javascript/components/people_dashboard/PeopleDashboardApp.js
+++ app/javascript/components/people_dashboard/PeopleDashboardApp.js
@@ -2,50 +2,49 @@ import React from 'react'
 import { observer } from 'mobx-react'
 import { subMonths, format, startOfMonth, endOfMonth } from 'date-fns'
 import PeopleDashboardStore from './PeopleDashboardStore'
-import WorkerSalary from './WorkerSalary';
-import HeadCountWidget from './HeadCountWidget';
+import WorkerSalary from './WorkerSalary'
+import HeadCountWidget from './HeadCountWidget'
 import AttritionWidget from './AttritionWidget'
 @observer
 class PeopleDashboardApp extends React.Component {
   constructor(props) {
-
-        super(props)
-        PeopleDashboardStore.loadWorkerSalary(this.props.facility_id)
-        PeopleDashboardStore.loadheadCount(this.props.facility_id)
+    super(props)
+    PeopleDashboardStore.loadWorkerSalary(this.props.facility_id)
+    PeopleDashboardStore.loadheadCount(this.props.facility_id)
+    PeopleDashboardStore.loadAttrition(this.props.facility_id, "", 2019)
   }
-    
-    render(){
-        return(
-            <React.Fragment>
-                <div className="flex mt4 h-50">
-                    <div className="w-30">
-                        <div
-                            className="ba b--light-gray pa3 bg-white br2 mr3"
-                            style={{ height: 300 + 'px' }}
-                            >
-                            <WorkerSalary />
-                        </div>
-                    </div>
-                    <div className="w-60">
-                        <div
-                            className="ba b--light-gray pa3 bg-white br2 mr3"
-                            style={{ height: 300 + 'px' }}
-                            >
-                            <AttritionWidget />
-                        </div>
-                    </div>
-                    <div className="w-30">
-                        <div
-                            className="ba b--light-gray pa3 bg-white br2 mr3"
-                            style={{ height: 300 + 'px' }}
-                            >
-                            <HeadCountWidget />
-                        </div>
-                    </div>
-                    
-                </div>
-            </React.Fragment>
-        )
-    }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="flex mt4 h-50">
+          <div className="w-30">
+            <div
+              className="ba b--light-gray pa3 bg-white br2 mr3"
+              style={{ height: 320 + 'px' }}
+            >
+              <WorkerSalary />
+            </div>
+          </div>
+          <div className="w-60">
+            <div
+              className="ba b--light-gray pa3 bg-white br2 mr3"
+              style={{ height: 320 + 'px' }}
+            >
+              <AttritionWidget />
+            </div>
+          </div>
+          <div className="w-30">
+            <div
+              className="ba b--light-gray pa3 bg-white br2 mr3"
+              style={{ height: 320 + 'px' }}
+            >
+              <HeadCountWidget />
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    )
+  }
 }
 export default PeopleDashboardApp

63b3ba38930d864e3b48be10cd48b59dd2af231f
Fatin Farhana Badrul Hisham <fatinfarhana.badrulhisham@gmail.com>
2019-08-07 16:47:11 +0800

    update with prettier-eslint

--- app/javascript/components/people_dashboard/CapacityPlanning.js
+++ app/javascript/components/people_dashboard/CapacityPlanning.js
@@ -18,21 +18,40 @@ const MenuButton = ({ icon, text, onClick, className = '' }) => {
   )
 }
 
-const PlanningCapacity = ({ capacity, actual, height, job, color, onClick }) => {
+const PlanningCapacity = ({
+  capacity,
+  actual,
+  height,
+  job,
+  color,
+  onClick
+}) => {
   return (
     <div
       className="mr2 grow dib"
       onClick={onClick}
       style={{ cursor: 'pointer', width: '70px' }}
     >
-      <div className='tc' style={{ height: '120px', background: `${color}` }}>
+      <div className="tc" style={{ height: '120px', background: `${color}` }}>
         <div
-          className='tc'
+          className="tc"
           style={{ height: `${height}%`, background: 'white', opacity: 0.5 }}
         >
-          {(height > 60.5) ? ((<span className='f6 fw6 black '>{actual} / {capacity} Hours</span>)) : ('')}
+          {height > 60.5 ? (
+            <span className="f6 fw6 black ">
+              {actual} / {capacity} Hours
+            </span>
+          ) : (
+            ''
+          )}
         </div>
-        {(height <= 60.5) ? (<span className='f6 fw6 white'>{actual} / {capacity} Hours</span>) : ('')}
+        {height <= 60.5 ? (
+          <span className="f6 fw6 white">
+            {actual} / {capacity} Hours
+          </span>
+        ) : (
+          ''
+        )}
       </div>
       <div>
         <h1 className="f5 fw6 grey ttc tc">{job}</h1>
@@ -109,7 +128,7 @@ export default class OverallInfo extends React.Component {
               </div>
             </Tippy>
           </div>
-          { PeopleDashboardStore.capacity_planning_loaded ? (
+          {PeopleDashboardStore.capacity_planning_loaded ? (
             <div className="flex justify-between mb3">
               {PeopleDashboardStore.data_capacity_planning.map((e, i) => (
                 <PlanningCapacity
@@ -135,8 +154,6 @@ export default class OverallInfo extends React.Component {
             )}
           </div>
         </div>
-
-
       </React.Fragment>
     )
   }
--- app/javascript/components/people_dashboard/WorkerList.js
+++ app/javascript/components/people_dashboard/WorkerList.js
@@ -20,7 +20,7 @@ const ProgressBar = React.memo(
     return (
       <div
         className={`bg-moon-gray br-pill overflow-y-hidden dib ${className}`}
-        style={{ height: height, width: '300px'}}
+        style={{ height: height, width: '300px' }}
       >
         <div className={` br-pill shadow-1 ${barColor}`} style={style} />
       </div>
@@ -49,7 +49,10 @@ class OntimeArrivalsWidget extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <h1 className="f5 fw6 black">{PeopleDashboardStore.data_worker_lists.title} - {PeopleDashboardStore.current_workers_length} Workers</h1>
+        <h1 className="f5 fw6 black">
+          {PeopleDashboardStore.data_worker_lists.title} -{' '}
+          {PeopleDashboardStore.current_workers_length} Workers
+        </h1>
         <div className="flex justify-between mb2">
           <div className=" flex items-center w-20">
             <h1 className="f5 fw6 gray">Worker</h1>
@@ -64,33 +67,35 @@ class OntimeArrivalsWidget extends React.Component {
             <h1 className="f5 fw6 gray">Action</h1>
           </div>
         </div>
-        {PeopleDashboardStore.data_worker_lists.users.map((e,i) => (
+        {PeopleDashboardStore.data_worker_lists.users.map((e, i) => (
           <div className="flex justify-between mb3">
             <div className="flex items-center w-20">
-            {e.photo_url ? (
-              <div>
-                <img
-                  src={e.photo_url}
-                  style={{
-                    width: '36px',
-                    height: '36px',
-                    borderRadius: '18px'
-                  }}
-                  onError={x => {
-                    x.target.onerror = null
-                    x.target.src = DefaultAvatar
-                  }}
+              {e.photo_url ? (
+                <div>
+                  <img
+                    src={e.photo_url}
+                    style={{
+                      width: '36px',
+                      height: '36px',
+                      borderRadius: '18px'
+                    }}
+                    onError={x => {
+                      x.target.onerror = null
+                      x.target.src = DefaultAvatar
+                    }}
+                  />
+                </div>
+              ) : (
+                <LetterAvatar
+                  firstName={e.first_name}
+                  lastName={e.last_name}
+                  size={36}
+                  radius={18}
                 />
-              </div>
-            ) : (
-              <LetterAvatar
-                firstName={e.first_name}
-                lastName={e.last_name}
-                size={36}
-                radius={18}
-              />
-            )}
-              <span className="f6 fw6 dark-grey ml2">{e.first_name} {e.last_name}</span>
+              )}
+              <span className="f6 fw6 dark-grey ml2">
+                {e.first_name} {e.last_name}
+              </span>
             </div>
             <div className="flex items-center w-50">
               <ProgressBar
@@ -99,15 +104,24 @@ class OntimeArrivalsWidget extends React.Component {
                 height={10}
                 barColor={this.getProgressBarColor(e.actual, e.capacity)}
               />
-              <span className="f6 fw6 dark-grey ml2">{e.actual} / {e.capacity} Hours</span>
+              <span className="f6 fw6 dark-grey ml2">
+                {e.actual} / {e.capacity} Hours
+              </span>
             </div>
             <div className="w-20">
-              {e.skills.map(skill =>
-                <button className="button mr3 bg-gray bn pa2 tc f6 white">{skill}</button>
-              )}
+              {e.skills.map(skill => (
+                <button className="button mr3 bg-gray bn pa2 tc f6 white">
+                  {skill}
+                </button>
+              ))}
             </div>
-            <div className="w-10" href="#" style={{ cursor: 'pointer'}}>
-              <button className="button mr3 bg-orange bn pa2 tc f5 white" style={{ cursor: 'pointer'}}>Assign Task</button>
+            <div className="w-10" href="#" style={{ cursor: 'pointer' }}>
+              <button
+                className="button mr3 bg-orange bn pa2 tc f5 white"
+                style={{ cursor: 'pointer' }}
+              >
+                Assign Task
+              </button>
             </div>
           </div>
         ))}

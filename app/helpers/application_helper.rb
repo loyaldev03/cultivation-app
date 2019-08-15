caab8f75787a6c90627381e00fc98977869a53e8
fathiabdulrahim <fathiabdrahim@yahoo.com>
2019-08-15 09:44:49 +0800

    move seeds and purchased clones tab to plants page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -38,14 +38,14 @@ module ApplicationHelper
 
   def active_link(path)
     if path == 'plant'
-      paths = [mothers_inventory_plants_path, cultivation_batches_inventory_plants_path, clones_inventory_plants_path, vegs_inventory_plants_path, flowers_inventory_plants_path, harvests_inventory_plants_path, harvest_batches_inventory_plants_path]
+      paths = [mothers_inventory_plants_path, cultivation_batches_inventory_plants_path, clones_inventory_plants_path, vegs_inventory_plants_path, flowers_inventory_plants_path, harvests_inventory_plants_path, harvest_batches_inventory_plants_path, seeds_inventory_plants_path, purchased_clones_inventory_plants_path]
       if paths.include?(request.path)
         'active'
       else
         'grey'
       end
     elsif path == 'raw_material'
-      paths = [nutrients_inventory_raw_materials_path, grow_medium_inventory_raw_materials_path, grow_lights_inventory_raw_materials_path, supplements_inventory_raw_materials_path, others_inventory_raw_materials_path, seeds_inventory_raw_materials_path, purchased_clones_inventory_raw_materials_path]
+      paths = [nutrients_inventory_raw_materials_path, grow_medium_inventory_raw_materials_path, grow_lights_inventory_raw_materials_path, supplements_inventory_raw_materials_path, others_inventory_raw_materials_path]
       if paths.include?(request.path)
         'active'
       else
@@ -64,8 +64,8 @@ module ApplicationHelper
       paths = [dashboard_cultivation_batches_path, cult_batches_path, cult_plants_path, cult_harvests_path, cult_tasks_path, cult_issues_path]
     when 'inventory'
       paths = [inventory_strains_path, mothers_inventory_plants_path, nutrients_inventory_raw_materials_path, products_inventory_sales_products_path, inventory_metrc_index_path, convert_products_inventory_sales_products_path]
-      plant_paths = [mothers_inventory_plants_path, cultivation_batches_inventory_plants_path, clones_inventory_plants_path, vegs_inventory_plants_path, flowers_inventory_plants_path, harvests_inventory_plants_path, harvest_batches_inventory_plants_path]
-      raw_material_path = [nutrients_inventory_raw_materials_path, grow_medium_inventory_raw_materials_path, grow_lights_inventory_raw_materials_path, supplements_inventory_raw_materials_path, others_inventory_raw_materials_path, seeds_inventory_raw_materials_path, purchased_clones_inventory_raw_materials_path]
+      plant_paths = [mothers_inventory_plants_path, cultivation_batches_inventory_plants_path, clones_inventory_plants_path, vegs_inventory_plants_path, flowers_inventory_plants_path, harvests_inventory_plants_path, harvest_batches_inventory_plants_path, seeds_inventory_plants_path, purchased_clones_inventory_plants_path]
+      raw_material_path = [nutrients_inventory_raw_materials_path, grow_medium_inventory_raw_materials_path, grow_lights_inventory_raw_materials_path, supplements_inventory_raw_materials_path, others_inventory_raw_materials_path]
       paths = paths + plant_paths + raw_material_path
     when 'people'
       paths = [employees_dashboard_path, employees_path, employees_schedule_path, timesheets_path, requests_path]

6de9ba1a7188fd18b635993d630f8b0a4e7f7a6f
Fatin Farhana Badrul Hisham <fatinfarhana.badrulhisham@gmail.com>
2019-09-03 17:05:27 +0800

    update new UI and fix edit form

--- app/forms/facilities_form/row_update.rb
+++ app/forms/facilities_form/row_update.rb
@@ -13,17 +13,15 @@ module FacilitiesForm
     validate :code_must_be_unique
 
     def self.find(id)
-      facility = Facility.where('rooms.sections.rows._id' => BSON::ObjectId.from_string(id)).first
+      facility = Facility.where('rooms.rows._id' => BSON::ObjectId.from_string(id)).first
       bson_id = BSON::ObjectId.from_string(id)
       row = nil
 
       facility.rooms.each do |r|
-        r.sections.each do |s|
-          s.rows.each do |rw|
-            if rw.id == bson_id
-              row = rw
-              break
-            end
+        r.rows.each do |rw|
+          if rw.id == bson_id
+            row = rw
+            break
           end
         end
       end
@@ -42,7 +40,7 @@ module FacilitiesForm
     end
 
     def code_must_be_unique
-      if row.section.rows.any? { |r| r.code == code && r.id != id }
+      if row.room.rows.any? { |r| r.code == code && r.id != id }
         errors.add(:code, 'must be unique')
       end
     end

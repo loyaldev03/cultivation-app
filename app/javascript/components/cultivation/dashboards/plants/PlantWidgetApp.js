297a07f91659d2eba77ee0053517410723f933cc
Anis Soraya <anissoraya1028@gmail.com>
2019-09-04 19:53:45 +0800

    prettier

--- app/javascript/components/cultivation/dashboards/plants/PlantWidgetApp.js
+++ app/javascript/components/cultivation/dashboards/plants/PlantWidgetApp.js
@@ -22,24 +22,24 @@ class PlantWidgetApp extends React.Component {
           </div>
         </div>
         <div className="w-50">
-            <div
-              className="ba b--light-gray pa3 bg-white br2 mr3"
-              id="plantroom_chart"
-              style={{ height: 400 + 'px' }}
-            >
-              <h1 className="f5 fw6 dark-grey">Plant Distribution by Room</h1>
-              <PlantByRoomWidget facility_id={this.props.facility_id} />
-            </div>
+          <div
+            className="ba b--light-gray pa3 bg-white br2 mr3"
+            id="plantroom_chart"
+            style={{ height: 400 + 'px' }}
+          >
+            <h1 className="f5 fw6 dark-grey">Plant Distribution by Room</h1>
+            <PlantByRoomWidget facility_id={this.props.facility_id} />
+          </div>
         </div>
         <div className="w-50">
-              <div
-                className="ba b--light-gray pa3 bg-white br2 mr3"
-                id="strain_chart"
-                style={{ height: 400 + 'px' }}
-              >
-                <h1 className="f5 fw6 dark-grey">Strain Distribution</h1>
-                <StrainDistribution url="/api/v1/dashboard_charts/strain_distribution" />
-              </div>
+          <div
+            className="ba b--light-gray pa3 bg-white br2 mr3"
+            id="strain_chart"
+            style={{ height: 400 + 'px' }}
+          >
+            <h1 className="f5 fw6 dark-grey">Strain Distribution</h1>
+            <StrainDistribution url="/api/v1/dashboard_charts/strain_distribution" />
+          </div>
         </div>
       </div>
     )

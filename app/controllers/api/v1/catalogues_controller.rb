87581162d7179a1f6aa60605488acbd9e936cd13
Andy Yong <kwokhou@gmail.com>
2019-09-04 17:48:01 +0800

    Added API to update catalogue active status

--- app/controllers/api/v1/catalogues_controller.rb
+++ app/controllers/api/v1/catalogues_controller.rb
@@ -18,4 +18,16 @@ class Api::V1::CataloguesController < Api::V1::BaseApiController
     records = Inventory::CatalogueSerializer.new(records, options)
     render json: records.serialized_json
   end
+
+  def update
+    record = Inventory::Catalogue.find(params[:id])
+    record.is_active = params[:is_active] == true
+    record.save
+
+    options = {params: {uoms: record.uoms.to_a}}
+    render json: Inventory::CatalogueSerializer.new(
+      record,
+      options,
+    ).serialized_json
+  end
 end

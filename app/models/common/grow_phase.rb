56fe14f82ac3aa14e87cd9bc89af26504dbf7044
Fatin Farhana Badrul Hisham <fatinfarhana.badrulhisham@gmail.com>
2019-09-05 15:32:56 +0800

    change column number_of_days_avg type

--- app/models/common/grow_phase.rb
+++ app/models/common/grow_phase.rb
@@ -6,7 +6,7 @@ module Common
     field :name, type: String
     field :is_active, type: Boolean, default: true
     field :number_of_days, type: Integer
-    field :number_of_days_avg, type: Integer
+    field :number_of_days_avg, type: Float, default: -> { 0 }
 
     scope :active, -> { where(is_active: true) }
   end

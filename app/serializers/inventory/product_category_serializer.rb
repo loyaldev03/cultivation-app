aa173c85d7346418673db7bf93b9e392b193d965
Andy Yong <kwokhou@gmail.com>
2019-09-11 16:49:12 +0800

    Update ProductCategory serializer to return package units

--- app/serializers/inventory/product_category_serializer.rb
+++ app/serializers/inventory/product_category_serializer.rb
@@ -10,11 +10,13 @@ module Inventory
 
     attribute :sub_categories do |object|
       object.sub_categories.map do |sub|
+        package_units = sub.package_units.map { |u| { value: u.value, label: u.label } }
         {
           id: sub.id.to_s,
           name: sub.name,
           product_category_id: object.id.to_s,
           product_category_name: object.name,
+          package_units: package_units
         }
       end
     end

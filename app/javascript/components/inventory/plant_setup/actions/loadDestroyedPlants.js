50a84cfda0a9d7bfb6470c2253d7e248ff59891c
Anis Soraya <anissoraya1028@gmail.com>
2019-09-11 11:47:18 +0800

    added destroyed plants dashboard and waste plant page

---
+++ app/javascript/components/inventory/plant_setup/actions/loadDestroyedPlants.js
@@ -0,0 +1,37 @@
+import plantStore from '../store/PlantStore'
+import isEmpty from 'lodash.isempty'
+import { httpGetOptions } from '../../../utils'
+
+/**
+ * Resets and reload list of plants
+ */
+export default function loadDestroyedPlants(
+
+  facility_id = '',
+  excludes = []
+) {
+  plantStore.isLoading = true
+  let apiUrl = '/api/v1/plants/all_destroyed_plant'
+
+  if (facility_id && facility_id.length > 0) {
+    apiUrl = apiUrl + '?facility_id=' + facility_id
+  }
+
+  fetch(apiUrl, httpGetOptions)
+    .then(response => {
+      return response.json().then(data => {
+        return {
+          status: response.status,
+          data: data.data
+        }
+      })
+    })
+    .then(({ status, data }) => {
+      if (status >= 400) {
+        console.log('Something wrong when calling /api/v1/plants/all_destroyed_plants')
+      } else {
+        plantStore.load(data)
+        plantStore.isLoading = false
+      }
+    })
+}

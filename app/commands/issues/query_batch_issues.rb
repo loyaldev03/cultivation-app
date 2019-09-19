310449dc24450f2f18622085efc0334b8ce91025
Fatin Farhana Badrul Hisham <fatinfarhana.badrulhisham@gmail.com>
2019-09-19 10:20:07 +0800

    update query_batch_issues

--- app/commands/issues/query_batch_issues.rb
+++ app/commands/issues/query_batch_issues.rb
@@ -12,7 +12,7 @@ module Issues
     def call
       Issues::Issue.where(cultivation_batch_id: batch_id, is_archived: false).
         in(status: filter_status).
-        order(created_at: :desc)
+        order(created_at: :desc).includes(:task, :cultivation_batch, :reported_by, :assigned_to, :resolved_by)
     end
   end
 end

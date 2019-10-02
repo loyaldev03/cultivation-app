0122cc03ad61dbdb17aed54d0effbce15b7d35ad
Anis Soraya <anissoraya1028@gmail.com>
2019-10-02 13:56:43 +0800

    Update Plants table to load data through API but for vegs (veg / veg1 / veg2)

--- app/javascript/components/inventory/plant_setup/VegSetupApp.js
+++ app/javascript/components/inventory/plant_setup/VegSetupApp.js
@@ -22,9 +22,12 @@ class VegSetupApp extends React.Component {
           accessor: 'id',
           show: false
         },
-        { Header: 'Plant ID', accessor: 'plant_id', show: false },
         {
-          Header: 'Plant',
+          accessor: 'plant_id',
+          show: false
+        },
+        {
+          Header: 'Plant ID',
           accessor: 'plant_tag',
           headerStyle: { textAlign: 'left' },
           Cell: x => (
@@ -41,28 +44,19 @@ class VegSetupApp extends React.Component {
           Header: (
             <HeaderFilter
               title="Batch ID"
-              accessor="cultivation_batch_name"
+              accessor="cultivation_batch"
               getOptions={PlantStore.getUniqPropValues}
               onUpdate={PlantStore.updateFilterOptions}
             />
           ),
-          accessor: 'cultivation_batch_name',
+          accessor: 'cultivation_batch',
           headerStyle: { textAlign: 'left' },
           Cell: props => <span>{props.value || 'Unnamed Batch'}</span>
         },
         {
-          headerClassName: 'tl',
-          Header: (
-            <HeaderFilter
-              title="Strain"
-              accessor="strain_name"
-              getOptions={PlantStore.getUniqPropValues}
-              onUpdate={PlantStore.updateFilterOptions}
-            />
-          ),
+          Header: 'Strain',
           accessor: 'strain_name',
-          minWidth: 130,
-          Cell: props => <span className="truncate">{props.value}</span>
+          headerStyle: { textAlign: 'left' }
         },
         // {
         //   Header: 'Growth stage',
@@ -132,7 +126,7 @@ class VegSetupApp extends React.Component {
   componentDidMount() {
     const sidebarNode = document.querySelector('[data-role=sidebar]')
     window.editorSidebar.setup(sidebarNode)
-    loadPlants('veg', '', this.props.facility_id)
+    //loadPlants('veg', '', this.props.facility_id)
   }
 
   openSidebar(event, id) {
@@ -148,6 +142,22 @@ class VegSetupApp extends React.Component {
           x.Header === column.Header ? column : x
         )
       })
+    }
+  }
+
+  onFetchData = (state, instance) => {
+    PlantStore.setFilter({
+      facility_id: this.props.facility_id,
+      current_growth_stage: this.props.current_growth_stage,
+      page: state.page,
+      limit: state.pageSize
+    })
+    PlantStore.loadPlants()
+  }
+
+  onSave = payload => {
+    if (payload) {
+      PlantStore.loadPlants()
     }
   }
 
@@ -178,7 +188,7 @@ class VegSetupApp extends React.Component {
               className="input w5"
               placeholder="Search Plants"
               onChange={e => {
-                PlantStore.filter = e.target.value
+                PlantStore.searchTerm = e.target.value
               }}
             />
             <CheckboxSelect options={columns} onChange={this.onToggleColumns} />
@@ -186,9 +196,11 @@ class VegSetupApp extends React.Component {
 
           <div className="pv3">
             <ListingTable
-              columns={columns}
+              ajax={true}
+              onFetchData={this.onFetchData}
               data={PlantStore.filteredList}
-              className="f6 -highlight"
+              pages={PlantStore.metadata.pages}
+              columns={columns}
               isLoading={PlantStore.isLoading}
             />
           </div>
@@ -198,6 +210,7 @@ class VegSetupApp extends React.Component {
           cultivation_batches={this.props.cultivation_batches}
           scanditLicense={this.props.scanditLicense}
           facility_id={this.props.facility_id}
+          onSave={this.onSave}
           canUpdate={plantPermission.update}
           canCreate={plantPermission.create}
         />

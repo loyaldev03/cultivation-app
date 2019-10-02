abf8c0116739b9e7704fc20d837a5a276077f0d5
Andy Yong <kwokhou@gmail.com>
2019-10-02 15:27:23 +0800

    Rescue error while creating notificaion - duplicate index

--- app/workers/create_notifications_worker.rb
+++ app/workers/create_notifications_worker.rb
@@ -40,6 +40,11 @@ class CreateNotificationsWorker
       }
       rec = Notification.new(notification)
       rec.save
+    rescue Mongo::Error::OperationFailure => e
+      if Rollbar
+        Rollbar.info("Duplicate notification. Skipped.")
+      end
+      next
     end
   end
 

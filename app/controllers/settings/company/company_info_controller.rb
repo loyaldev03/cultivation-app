08183ba24a45532965bb5f39fd26ad9c960484a6
Andy Yong <kwokhou@gmail.com>
2019-10-08 15:06:11 +0800

    Fix Onboarding Seed & Preference

--- app/controllers/settings/company/company_info_controller.rb
+++ app/controllers/settings/company/company_info_controller.rb
@@ -85,7 +85,7 @@ class Settings::Company::CompanyInfoController < ApplicationController
     end
 
     if @company_info.enable_metrc_integration &&
-       @company_info.metrc_user_key
+        @company_info.metrc_user_key
       MetrcOnboardingWorker.perform_async
     end
 
@@ -112,8 +112,14 @@ class Settings::Company::CompanyInfoController < ApplicationController
     if @company_info.save
       flash[:notice] = 'Company info saved'
       if params[:onboarding_type].present?
-        @facility = Facility.find(params[:facility_id])
-        @facility.update_onboarding('ONBOARDING_COMP_INFO')
+        Facility.all.each do |f|
+          if f.preferences.blank?
+            Common::SeedOnboardingPreference.call(f.id)
+          end
+        end
+        Facility.all.each do |f|
+          f.update_onboarding('ONBOARDING_COMP_INFO')
+        end
       end
       redirect_to edit_settings_company_company_info_path(
         type: params[:type],

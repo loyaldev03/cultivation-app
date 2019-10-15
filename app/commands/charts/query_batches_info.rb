6612f0dd0423bbf7089c01540e9f27b99d8cf5bc
Anis Soraya <anissoraya1028@gmail.com>
2019-10-15 11:52:59 +0800

    fix batches number widget

--- app/commands/charts/query_batches_info.rb
+++ app/commands/charts/query_batches_info.rb
@@ -14,19 +14,23 @@ module Charts
       #unscheduled batches
       #cost of active batches to date
       facilities = @args[:facility_id].split(',')
-      batches = Cultivation::Batch.where(facility_id: {"$in": facilities})
-
-      active_batches = batches.select { |a| a[:status] == Constants::BATCH_STATUS_ACTIVE }.count
-      draft_batches = batches.select { |a| a[:status] == Constants::BATCH_STATUS_DRAFT }.count
-      scheduled_batches = batches.select { |a| a[:status] == Constants::BATCH_STATUS_SCHEDULED }.count
+      batches = Cultivation::Batch.collection.aggregate([
+        {"$match": {"facility_id": {"$in": facilities.map { |x| x.to_bson_id }}}},
+        {"$group": {_id: '$status', total_batches: {"$sum": 1}}},
+      ])
+      #return batches
+      active_batches = batches.select { |a| a[:_id] == Constants::BATCH_STATUS_ACTIVE }.first
+      draft_batches = batches.select { |a| a[:_id] == Constants::BATCH_STATUS_DRAFT }.first
+      scheduled_batches = batches.select { |a| a[:_id] == Constants::BATCH_STATUS_SCHEDULED }.first
 
       active_batches_cost = Charts::QueryActiveBatchesCost.call(@args[:current_user], {facility_id: facilities}).result
       json = {
-        active_batches: active_batches,
-        draft_batches: draft_batches,
-        scheduled_batches: scheduled_batches,
+        active_batches: active_batches.present? ? active_batches[:total_batches] : 0,
+        draft_batches: draft_batches.present? ? draft_batches[:total_batches] : 0,
+        scheduled_batches: scheduled_batches.present? ? scheduled_batches[:total_batches] : 0,
         active_batches_cost: active_batches_cost,
       }
+      return json
     end
   end
 end

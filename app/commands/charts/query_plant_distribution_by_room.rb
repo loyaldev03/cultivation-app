1603b49cd1f834d664affa54d60fe27172174001
Anis Soraya <anissoraya1028@gmail.com>
2019-10-15 10:14:26 +0800

    performance on plant by room widget

--- app/commands/charts/query_plant_distribution_by_room.rb
+++ app/commands/charts/query_plant_distribution_by_room.rb
@@ -5,54 +5,63 @@ module Charts
     def initialize(current_user, args = {})
       @user = current_user
       @args = args
-      @facility_id = @args[:facility_id].split(',')
+      @facility_id = args[:facility_id]
     end
 
     def call
-      if resource_shared?
-        facilities = Facility.in(id: active_facility_ids)
-      else
-        facilities = Facility.in(id: @facility_id)
+
+      locations = QueryLocations.call(@facility_id)
+      plants = Inventory::Plant.collection.aggregate([
+        {"$lookup": {
+          from: 'inventory_facility_strains',
+          localField: 'facility_strain_id',
+          foreignField: '_id',
+          as: 'facility_strain',
+        }},
+        {"$unwind": {path: '$facility_strain', preserveNullAndEmptyArrays: true}},
+        {"$match": {"facility_strain.facility_id": {"$in": @facility_id}}},
+        {"$project": {
+          "plant_id": 1,
+          "location_id": 1
+          
+        }}
+      
+      ])
+
+      group_plants = plants.group_by do |a|
+          location = locations.query_trays(a[:location_id])
+          location&.first&.first[:row_purpose] if location&.first&.first.present? and location&.first&.first[:row_purpose].present?
       end
 
       json_fac = []
-      facilities.each do |facility|
-        facility_strain_ids = facility.strains.map { |a| a.id.to_s }
-        plants = Inventory::Plant.where(facility_strain_id: {'$in': facility_strain_ids})
-        locations = QueryLocations.call([facility.id])
-        #raise ""
-        group_plants = plants.group_by do |a|
-          location = locations.query_trays(a.location_id)
-          location&.first&.first[:row_purpose] if location&.first&.first.present? and location&.first&.first[:row_purpose].present?
-        end
-        json_fac << {
-          group_plant: group_plants.map do |n|
-            {
-              group: n[0],
-              count: n[1].count,
-            }
-          end,
-        }
-      end
+
+      json_fac << {
+        group_plant: group_plants.map do |n|
+          {
+            group: n[0],
+            count: n[1].count,
+          }
+        end,
+      }
+
       array = json_fac.inject { |memo, el| memo.merge(el) { |k, old_v, new_v| old_v + new_v } }
 
-      #return array
       if array.present?
-        array2 = array.map { |x| x[1] }
-
-        group_count = array2.inject { |memo, el| memo.merge(el) { |k, old_v, new_v| old_v + new_v } }
-
-        grouped_plant_json = group_count.group_by { |x| x[:group] }.map do |f|
-          if f[0].present?
-            {
-              name: f[0],
-              value: f[1].map { |x| x[:count] }.sum,
-            }
+          array2 = array.map { |x| x[1] }
+  
+          group_count = array2.inject { |memo, el| memo.merge(el) { |k, old_v, new_v| old_v + new_v } }
+  
+          grouped_plant_json = group_count.group_by { |x| x[:group] }.map do |f|
+            if f[0].present?
+              {
+                name: f[0],
+                value: f[1].map { |x| x[:count] }.sum,
+              }
+            end
           end
+        else
+          grouped_plant_json = []
         end
-      else
-        grouped_plant_json = []
-      end
 
       return {children: grouped_plant_json.compact}
     end

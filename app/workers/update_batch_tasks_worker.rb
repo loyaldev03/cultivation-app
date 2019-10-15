3e2a20dc313d4e915a2be5c6264664d9fc37fa34
Andy Yong <kwokhou@gmail.com>
2019-10-15 11:23:27 +0800

    Added new worker to update task fields

---
+++ app/workers/update_batch_tasks_worker.rb
@@ -0,0 +1,21 @@
+class UpdateBatchTasksWorker
+  include Sidekiq::Worker
+
+  def perform(batch_id)
+    @batch_id = batch_id
+
+    batch_tasks.each do |task|
+      task.update_attribute(:assignable, task.have_children(batch_tasks))
+    end
+  end
+
+  private
+
+  def batch
+    batch ||= Cultivation::Batch.find(@batch_id)
+  end
+
+  def batch_tasks
+    batch_tasks ||= Cultivation::QueryTasks.call(batch).result
+  end
+end

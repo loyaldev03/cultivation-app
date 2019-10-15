1108f841b3aaf33b799185a7857b49d06bfe6cb6
fathiabdulrahim <fathiabdrahim@yahoo.com>
2019-10-15 13:31:09 +0800

    change query to aggregate for harvest cost

--- app/commands/charts/query_harvest_cost.rb
+++ app/commands/charts/query_harvest_cost.rb
@@ -9,31 +9,55 @@ module Charts
     end
 
     def call
-      batches = Cultivation::Batch.in(facility_id: @facility_id)
-      harvest_batches = Inventory::HarvestBatch.in(cultivation_batch_id: batches.map { |a| a.id.to_s }).includes(:cultivation_batch)
-      sum_cost = 0
-      harvest_json = harvest_batches.map do |a|
-        cost = (a.cultivation_batch.actual_labor_cost.to_f + a.cultivation_batch.actual_material_cost.to_f) / a.total_wet_weight.to_f
-        cost = a.total_wet_weight == 0.0 ? 0.0 : cost
-        sum_cost += cost
-        {
-          id: a.id.to_s,
-          harvest_batch: a.harvest_name,
-          cost: cost.round(2),
-        }
+      batch_ids = Cultivation::Batch.in(facility_id: @facility_id).pluck(:id)
+
+      if @args[:order].present? && @args[:order] == 'top'
+        order = -1
+      else
+        order = 1
       end
 
-      if @args[:order].present?
-        if @args[:order] == 'top'
-          harvest_json = harvest_json.sort_by { |a| -a[:cost] }
-        else
-          harvest_json = harvest_json.sort_by { |a| a[:cost] }
-        end
-      end
+      result = Inventory::HarvestBatch.collection.aggregate([
+        {"$match": {"cultivation_batch_id": {"$in": batch_ids}}},
+
+        {"$lookup": {
+          from: 'cultivation_batches',
+          localField: 'cultivation_batch_id',
+          foreignField: '_id',
+          as: 'batch',
+        }},
+        {"$unwind": {path: '$batch', preserveNullAndEmptyArrays: true}},
+        {"$addFields": {"cost": {"$cond": [{"$gt": [{"$toDecimal": '$total_wet_weight'}, 0]}, {"$divide": [{"$add": ['$batch.actual_labor_cost', '$batch.actual_material_cost']}, {"$toDecimal": '$total_wet_weight'}]}, 0.0]}}},
+        {"$project": {
+          "id": {"$toString": '$_id'},
+          "harvest_batch": '$harvest_name',
+          "cost": {"$toDouble": '$cost'},
+        }},
+
+        {"$sort": {"cost": order}},
+
+      ]).to_a
+
+      average = Inventory::HarvestBatch.collection.aggregate([
+        {"$match": {"cultivation_batch_id": {"$in": batch_ids}}},
+
+        {"$lookup": {
+          from: 'cultivation_batches',
+          localField: 'cultivation_batch_id',
+          foreignField: '_id',
+          as: 'batch',
+        }},
+        {"$unwind": {path: '$batch', preserveNullAndEmptyArrays: true}},
+        {"$addFields": {"cost": {"$toDouble": {"$cond": [{"$gt": [{"$toDecimal": '$total_wet_weight'}, 0]}, {"$divide": [{"$add": ['$batch.actual_labor_cost', '$batch.actual_material_cost']}, {"$toDecimal": '$total_wet_weight'}]}, 0.0]}}}},
+        {"$group": {
+          "_id": 'null',
+          "average": {"$avg": '$cost'},
+        }},
+      ]).to_a
 
       {
-        average_harvest_cost: (sum_cost.to_f / harvest_batches.size).round(2),
-        harvest_cost: harvest_json,
+        average_harvest_cost: average[0].present? ? average[0]['average'].round(2) : 0.00,
+        harvest_cost: result,
       }
     end
   end

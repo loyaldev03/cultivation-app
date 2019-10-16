594cb272256d06f5eeb50cbf06d5935833bdd2c7
Fatin Farhana Badrul Hisham <fatinfarhana.badrulhisham@gmail.com>
2019-10-16 10:19:52 +0800

    fix issues list  widget

--- app/javascript/components/manager_dashboard/IssueList.js
+++ app/javascript/components/manager_dashboard/IssueList.js
@@ -4,6 +4,7 @@ import ChartStore from './ChartStore'
 import { observer } from 'mobx-react'
 import { formatIssueNo } from '../issues/components/FormatHelper'
 import { formatDate, formatTime } from '../utils/DateHelper'
+import isEmpty from 'lodash.isempty'
 
 @observer
 export default class UnassignedTask extends React.Component {
@@ -26,27 +27,36 @@ export default class UnassignedTask extends React.Component {
         </div>
 
         <div className="overflow-y-scroll" style={{ height: 320 + 'px' }}>
-          {ChartStore.filterIssueList.map(e => (
+          {ChartStore.issue_list_loaded &&
+          !isEmpty(ChartStore.filterIssueList) ? (
             <React.Fragment>
-              <div className="mb4">
-                <a
-                  className="no-underline"
-                  href={`/cultivation/batches/${e.cultivation_batch_id}/issues`}
-                >
-                  <span className="f5 grey">
-                    Issue {formatIssueNo(e.issue_no)}, Batch {e.batch_no}{' '}
-                  </span>
-                  <span className="f7 green ttu">{e.status}</span>
-                  <br />
-                  <span className="f7 mt1 grey">
-                    {formatDate(e.c_at)}, {formatTime(e.c_at)}
-                  </span>
-                  <br />
-                  <span className="dark-grey">{e.title}</span>
-                </a>
-              </div>
+              {ChartStore.filterIssueList.map(e => (
+                <div className="mb4">
+                  <a
+                    className="no-underline"
+                    href={`/cultivation/batches/${e.cultivation_batch_id}/issues`}
+                  >
+                    <span className="f5 grey">
+                      Issue {formatIssueNo(e.issue_no)}, Batch {e.batch_no}{' '}
+                    </span>
+                    <span className="f7 green ttu">{e.status}</span>
+                    <br />
+                    <span className="f7 mt1 grey">
+                      {formatDate(e.c_at)}, {formatTime(e.c_at)}
+                    </span>
+                    <br />
+                    <span className="dark-grey">{e.title}</span>
+                  </a>
+                </div>
+              ))}
             </React.Fragment>
-          ))}
+          ) : (
+            <section className="mw5 mw7-ns center ph5-ns">
+              <p className="lh-copy measure tc grey f4">
+                No issues available
+              </p>
+            </section>
+          )}
         </div>
       </React.Fragment>
     )

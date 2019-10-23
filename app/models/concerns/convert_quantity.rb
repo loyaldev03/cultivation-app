9f6efb3b02dc3a409983e22359b9aedb44721338
fathiabdulrahim <fathiabdrahim@yahoo.com>
2019-10-23 16:11:20 +0800

    run formatter

--- app/models/concerns/convert_quantity.rb
+++ app/models/concerns/convert_quantity.rb
@@ -12,6 +12,6 @@ module ConvertQuantity
     Rails.logger.debug "\t\t\t>>>>> uom: #{uom}, catalogue: #{catalogue.common_uom}"
     temp_uom = Common::UnitOfMeasure.find_by(unit: uom)
     self.common_uom = catalogue.common_uom
-    self.common_quantity = temp_uom.to(quantity, catalogue.common_uom) if temp_uom.present?#convert quantity to common uom in catalogue
+    self.common_quantity = temp_uom.to(quantity, catalogue.common_uom) if temp_uom.present? #convert quantity to common uom in catalogue
   end
 end

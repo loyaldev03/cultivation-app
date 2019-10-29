32adcc970a3de654692a18d8bc632011b149e123
Fatin Farhana Badrul Hisham <fatinfarhana.badrulhisham@gmail.com>
2019-10-29 11:43:25 +0800

    update with prettier-eslint

--- app/javascript/components/issues/components/Comments.js
+++ app/javascript/components/issues/components/Comments.js
@@ -191,7 +191,7 @@ class Comments extends React.Component {
           })
         }
       })
-    }else{
+    } else {
       this.setState({
         showResolveForm: false,
         showAddComment: true
